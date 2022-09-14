fd3bb878008659dec1a537cf75b873a243b2bcd5
mgcooper <mguycooper@gmail.com>
2022-09-13 21:48:29 -0700

    added json autocomplete

--- manager/addproject/addproject.m
+++ manager/addproject/addproject.m
@@ -17,21 +17,32 @@ function projects = addproject(projectname,varargin)
    prjpath     = getprjsourcepath(projectname); % tb source path
    projects    = readprjdirectory(dbpath); % read the directory into memory
    
-   prjidx      = height(projects)+1;   
+   % this would be needed if i was appending it to the end of the table,
+   % but with buildprojectdirectory method, we just reubild it every time,
+   % in which case we need to get prjidx after adding it to send to tjhe
+   % subfunctions
+   %prjidx      = height(projects)+1;   
    
    if any(ismember(projects.name,projectname))
       
       error('project already in directory');
       
    else
-   
-      projects(prjidx,:)      = {projectname,prjpath};
 
       disp(['adding ' projectname ' to project directory']);
-
-      writeprjdirectory(projects,dbpath);
+      buildprojectdirectory;
+      
+      % this is based on the tb method which is more complicated, that
+      % directory only has two columns so i just add the name and path, but
+      % buildprojectdirectory is better, it just uses the outupt of dir(),
+      % so i just use buildprojectdirectory as above
+      %projects(prjidx,:) = {projectname,prjpath};
+      %writeprjdirectory(projects,dbpath);
    
    end
+   
+   projects = readprjdirectory(dbpath); % read the directory into memory
+   prjidx   = find(ismember(projects.name,projectname));
    
    % add it to the json directory choices for function 'activate'
    addtojsondirectory(projects,prjidx,'workon');
