9f8c380ccc4391a18090fcd9924233dac8188fc2
mgcooper <mguycooper@gmail.com>
2022-10-19 11:27:30 -0700

    removed default UTC time zone

--- libtime/mkcalendar.m
+++ libtime/mkcalendar.m
@@ -9,7 +9,8 @@ function T = mkcalendar(t1,t2,dt,varargin)
    p.addRequired(   't1',                 @(x)isnumeric(x)|isdatetime(x));
    p.addRequired(   't2',                 @(x)isnumeric(x)|isdatetime(x));
    p.addRequired(   'dt',                 @(x)ischar(x)|isduration(x)|iscalendarduration(x));
-   p.addParameter(  'TimeZone',  'UTC',   @(x)ischar(x));
+   p.addParameter(  'TimeZone',  'nan',   @(x)ischar(x));
+   %p.addParameter(  'TimeZone',  'UTC',   @(x)ischar(x));
    p.addParameter(  'noleap',    false,   @(x)islogical(x));
    p.parseMagically('caller');
 %--------------------------------------------------------------------------
@@ -23,7 +24,9 @@ function T = mkcalendar(t1,t2,dt,varargin)
    T = tocolumn(t1:dt:t2);
 
    % set the time zone
-   T.TimeZone = TimeZone;
+   if TimeZone ~= "nan"
+      T.TimeZone = TimeZone;
+   end
 
    % remove leap inds
    if noleap == true
