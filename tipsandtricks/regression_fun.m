05ddef55577d2cd8604b3391120d087a2535c2cc
mgcooper <mguycooper@gmail.com>
2022-10-27 14:09:36 -0700

    added reminder about matrix multiply notation

--- tipsandtricks/regression_fun.m
+++ tipsandtricks/regression_fun.m
@@ -10,6 +10,28 @@
 
 
 
+% I alwasy get confused by this so for reference:
+% this works:  [1 2] * [ones(1,10) ; rand(1,10)]
+% this works:  [1 2; 1 2; 1 2] * [ones(1,10) ; rand(1,10)]
+% this doesnt: [1 2; 1 2; 1 2] * rand(3,1)
+% this works:  [1 2; 1 2; 1 2]' * rand(3,1)
+% this works:  [1 2; 1 2; 1 2] .* rand(3,1)
+% this doesnt: [1; 2] * [ones(1,10);rand(1,10)]
+% the first one is standard linear regression type matrix multiplication: 
+% [a b] * [ 1  1  1  ]  =  [ a + b*t1 ]
+%         [ t1 t2 t3 ]     [ a + b*t2 ]
+%                          [ a + b*t3 ]
+%                          
+% the second one is two-linear regressions 
+% [a1 b1] * [ 1  1  1  ]  =   [ a1 + b1*t1 ]
+% [a2 b2]   [ t1 t2 t3 ]      [ a2 + b2*t1 ]
+% [a3 b3]                     [ a3 + b3*t1 ]
+%                             [ a1 + b1*t2 ]
+%                             [ a2 + b2*t2 ]
+%                             [ a3 + b3*t2 ]
+%                             [ a1 + b1*t3 ]
+%                             [ a2 + b2*t3 ]
+%                             [ a3 + b3*t3 ]
 
 
 
