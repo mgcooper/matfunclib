cc7ddaab1a52936296073e880240e85de5e76a3d
mgcooper <mguycooper@gmail.com>
2022-12-10 20:40:52 -0800

    update

--- libstruct/renameStructFields.m
+++ libstruct/renameStructFields.m
@@ -1,66 +1,69 @@
-function struct = renameStructFields(struct, oldFieldNames, newFieldNames)
-%RENAMESTRUCTFIELDS renames oldFieldName to newFieldName in struct str
+function S = renamestructfields(S, fields, newfields)
+%RENAMESTRUCTFIELDS renames oldfields to newfields in struct S
 %
-%   STR = RENAMESTRUCTFIELD(STR, OLDFIELDNAME, NEWFIELDNAME)
-%   STR is the struct in which to rename the field
-%   OLDFIELDNAME is the name of the field to rename
-%   NEWFIELDNAME is the name to rename OLDFIELDNAME to
+%   S = RENAMESTRUCTFIELD(S, FIELDS, NEWFIELDS)
+%   S is the struct in which to rename the fields
+%   FIELDS are the names of the fields to rename
+%   NEWFIELDS are the names to rename FIELDS to
 %
+%   Copyright 2013-2014 The MathWorks, Inc.
+% 
+%   mgc added the loop around multiple fields, reformatted for consistency
+%   with libstruct: renamed from renameStructField to renamestructfields,
+%   changed arguments 'struct', 'oldFieldNames', and 'newFieldNames' to 'S',
+%   'fields', and 'newfields'. added support for input char, string/array,
 
-%   mgc added the loop around multiple fieldnames
-
-if ischar(oldFieldNames)
-   oldFieldNames = cellstr(oldFieldNames);
+if ischar(fields) || isstring(fields)
+   fields = cellstr(fields);
 end
 
-if ischar(newFieldNames)
-   newFieldNames = cellstr(newFieldNames);
+if ischar(newfields) || isstring(newfields)
+   newfields = cellstr(newfields);
 end
 
 
-for n = 1:numel(oldFieldNames)
-    oldFieldName = oldFieldNames{n};
-    newFieldName = newFieldNames{n};
-
-if ~strcmp(oldFieldName, newFieldName)
-    allNames = fieldnames(struct);
-    % Is the user renaming one field to be the name of another field?
-    % Remember this.
-    isOverwriting = ~isempty(find(strcmp(allNames, newFieldName), 1));
-    matchingIndex = find(strcmp(allNames, oldFieldName));
-    if ~isempty(matchingIndex)
-        try
+for n = 1:numel(fields)
+   oldFieldName = fields{n};
+   newFieldName = newfields{n};
+   
+   if ~strcmp(oldFieldName, newFieldName)
+      allNames = fieldnames(S);
+      % Is the user renaming one field to be the name of another field?
+      % Remember this.
+      isOverwriting = ~isempty(find(strcmp(allNames, newFieldName), 1));
+      matchingIndex = find(strcmp(allNames, oldFieldName));
+      if ~isempty(matchingIndex)
+         try
             allNames{matchingIndex(1)} = newFieldName;
-            struct.(newFieldName) = struct.(oldFieldName);
-            struct = rmfield(struct, oldFieldName);
-        catch ME
+            S.(newFieldName) = S.(oldFieldName);
+            S = rmfield(S, oldFieldName);
+         catch ME
             if contains(ME.identifier,'scalarStrucRequired')
-                msg = 'non-scalar struct array detected';
-                causeException = MException('MATLAB:renameStructField2',msg);
-                ME = addCause(ME,causeException);
+               msg = 'non-scalar struct array detected';
+               causeException = MException('MATLAB:renameStructField2',msg);
+               ME = addCause(ME,causeException);
             end
             
-            tmp = struct.(oldFieldName);
+            tmp = S.(oldFieldName);
             if ~iscell(tmp)
-                [struct.(newFieldName)] = struct.(oldFieldName);
-                struct = rmfield(struct, oldFieldName);
-                
+               [S.(newFieldName)] = S.(oldFieldName);
+               S = rmfield(S, oldFieldName);
+               
             else
-                try
-                    struct.(newFieldName) = struct.(oldFieldName);
-                catch ME
-                   % need to test this 
-                end
+               try
+                  S.(newFieldName) = S.(oldFieldName);
+               catch ME
+                  % need to test this
+               end
             end
             
-        end
-        
-        if (~isOverwriting)
+         end
+         
+         if (~isOverwriting)
             % Do not attempt to reorder if we've reduced the number
             % of fields.  Bad things will result.  Let it go.
-            struct = orderfields(struct, allNames);
-        end
-    end
-end
-
+            S = orderfields(S, allNames);
+         end
+      end
+   end
 end
