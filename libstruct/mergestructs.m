3b26650933aa2c283186ee5853bcd1224bd6ba4a
mgcooper <mguycooper@gmail.com>
2023-06-18 19:50:16 -0700

    fix: try scalar struct merge, catch non-scalar

--- libstruct/mergestructs.m
+++ libstruct/mergestructs.m
@@ -23,16 +23,55 @@ function S = mergestructs(S, T)
 %   S       combination structure
 
 arguments
-   S(1,1) struct
-   T(1,1) struct
+   S(:,1) struct
+   T(:,1) struct
 end
 
+% % for octave compatibility, remove arguments block and use this:
+% if ~isstruct(S) || ~isstruct(T)
+%     error('input parameters contain non-struct');
+% end
+
+% try scalar merge, catch non-scalar
+try
+   S = mergescalarstructs(S,T);
+catch ME
+   if strcmp(ME.identifier,'MATLAB:scalarStrucRequired')
+      try
+         S = mergenonscalarstructs(S,T);
+      catch ME
+         rethrow(ME)
+      end
+   end
+end
+
+
+function S = mergescalarstructs(S,T)
 f = fieldnames(T);
-for i = 1:length(f)
-   if isfield(S, f{i})
-      warning("structtools:commonfield",...
-         "Field '%s' exists in both structures.", f{i});
+for n = 1:length(f)
+   if checkfield(S, f{n})
       continue
    end
-   S.(f{i}) = T.(f{i});
+   S.(f{n}) = T.(f{n});
+end
+
+function S = mergenonscalarstructs(S,T)
+f = fieldnames(T);
+if numel(S) == numel(T) && ~isscalar(S)
+   for n = 1:numel(f)
+      try % num2cell converts arrays of any type to cell, so may not need try
+         % not sure about num2cell vs {} vs mat2cell
+         % C = num2cell([T.(f{n})]);
+         C = {T.(f{n})};
+         [S(:).(f{n})] = deal(C{:});
+      catch
+      end
+   end
+end
+
+function tf = checkfield(S,f)
+tf = isfield(S, f);
+if tf
+   warning("mergestructs:commonfield",...
+      "Field '%s' exists in both structures.", f);
 end
