c5ba80cf2631db7b64856bd6d3babb955ae0f699
mgcooper <mguycooper@gmail.com>
2023-06-18 18:18:28 -0700

    refactor(liblogic): simplify true/false functions with direct tf = <logical statement> rather than if-else

--- liblogic/isaxis.m
+++ liblogic/isaxis.m
@@ -1,14 +1,7 @@
 function tf = isaxis(ax)
+%ISAXIS return true if ax is an axes object
+tf = isa(ax,'matlab.graphics.axis.Axes');
 
-% % based on plot.m functionsignature file, might use these:
-%    "type":[["matlab.graphics.axis.AbstractAxes"], ["matlab.ui.control.UIAxes"]]},
-   
-   tf = false;
-   if isa(ax,'matlab.graphics.axis.Axes')
-      tf = true;
-   end
-   
-%    % for reference, this was my first thought, didn't know about 'isa'
-%    if strcmp(class(ax),'matlab.graphics.axis.Axes')
-%       tf = true;
-%    end
+% % based on plot.m functionsignature file, might need these:
+% "type":[["matlab.graphics.axis.AbstractAxes"], ["matlab.ui.control.UIAxes"]]},
+
