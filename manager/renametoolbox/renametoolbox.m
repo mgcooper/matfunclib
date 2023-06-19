3af64e50a8b84f992a2fdcfdfae695deed5bd885
mgcooper <mguycooper@gmail.com>
2023-06-18 17:58:00 -0700

    feat: add ability to move/rename toolboxes into libraries with sub-libs

--- manager/renametoolbox/renametoolbox.m
+++ manager/renametoolbox/renametoolbox.m
@@ -1,25 +1,75 @@
-function toolboxes = renametoolbox(oldtbname,newtbname,varargin)
+function varargout = renametoolbox(oldtbname,newtbname,varargin)
 %RENAMETOOLBOX renames toolbox entry in toolboxdir and json files
+
+% NOTE!!! Need to add onCleanup and/or other methods to ensure the json file is
+% not overwritten and then fails to rewrite, this happened when i passed in
+% strings not chars, but I was able to copy deactivate version and recover
+
+% UPDATES
+% 11 Apr 2023, support for sublibs via 'libary' optional argument
+% 11 Apr 2023, support for moving source to sublib via 'movesource' namevalue
+
 %-------------------------------------------------------------------------
 p                 = inputParser;
 p.FunctionName    = mfilename;
 p.CaseSensitive   = false;
 p.KeepUnmatched   = true;
 
-addRequired(p,'oldtbname',@(x)ischar(x));
-addRequired(p,'newtbname',@(x)ischar(x));
+validlibs = @(x)any(validatestring(x,cellstr(gettbdirectorylist)));
+
+addRequired(p,'oldtbname',@(x)ischarlike(x));
+addRequired(p,'newtbname',@(x)ischarlike(x));
+addOptional(p,'library','',validlibs); % default value must be ''
 addParameter(p,'renamesource',false,@(x)islogical(x));
+addParameter(p,'movesource',false,@(x)islogical(x));
+addParameter(p,'force',false,@(x)islogical(x));
 
 parse(p,oldtbname,newtbname,varargin{:});
 oldtbname      = p.Results.oldtbname;
 newtbname      = p.Results.newtbname;
+libraryname    = p.Results.library;
 renamesource   = p.Results.renamesource;
+movesource     = p.Results.movesource;
+force          = p.Results.force;
+
+% 'renaming' is the same as 'moving' so combine them here
+renamesource = movesource | renamesource;
+
 %-------------------------------------------------------------------------
 
-dbpath      = gettbdirectorypath;
-toolboxes   = readtbdirectory(dbpath);
-tbidx       = findtbentry(toolboxes,oldtbname);
-tbpath      = gettbsourcepath(oldtbname);
+if isstring(oldtbname)
+   if isscalar(oldtbname)
+      oldtbname = char(oldtbname);
+   else
+      error('scalar string or char required')
+   end
+end
+
+if isstring(newtbname)
+   if isscalar(newtbname)
+      newtbname = char(newtbname);
+   else
+      error('scalar string or char required')
+   end
+end
+
+if isstring(libraryname)
+   if isscalar(libraryname)
+      libraryname = char(libraryname);
+   else
+      error('scalar string or char required')
+   end
+end
+
+
+% read the toolbox directory into memory
+toolboxes = readtbdirectory(gettbdirectorypath());
+
+% get the logical index for the toolbox entry
+tbidx = findtbentry(toolboxes,oldtbname);
+
+% set the path to the toolbox source code (works if args.library is empty)
+oldtbpath = gettbsourcepath(oldtbname);
 
 if not(any(tbidx))
 
@@ -27,14 +77,16 @@ if not(any(tbidx))
 
 else
 
-   newtbpath            = strrep(tbpath,oldtbname,newtbname);
-   toolboxes(tbidx,:)   = {newtbname,newtbpath};
+   % new method 11 Apr 2023 to move to sublib
+   %newtbpath = strrep(oldtbpath,oldtbname,newtbname);
+   newtbpath = fullfile(gettbsourcepath,libraryname,newtbname);
+   
+   toolboxes.name{tbidx} = newtbname;
+   toolboxes.source{tbidx} = newtbpath;
 
-   fprintf('\n toolbox %s renamed to %s \n',oldtbname,newtbname);
+   fprintf('\n toolbox %s renamed to %s/%s \n',oldtbname,libraryname,newtbname);
 
-%       disp(['renaming ' oldtbname ' to ' newtbname]);
-
-   writetbdirectory(toolboxes,dbpath);
+   writetbdirectory(toolboxes);
 
 end
 
@@ -45,8 +97,13 @@ renamejsondirectoryentry(oldtbname,newtbname,'activate');
 renamejsondirectoryentry(oldtbname,newtbname,'deactivate');
 
 % rename the source directory ?
-renametbsourcedir(renamesource,tbpath,newtbpath)
+renametbsourcedir(renamesource,oldtbpath,newtbpath,force)
 
+% output
+switch nargout
+   case 1
+      varargout{1} = toolboxes;
+end
 
 
 function renamejsondirectoryentry(oldtbname,newtbname,directoryname)
@@ -58,4 +115,6 @@ wholefile   = readtbjsonfile(jspath);
 wholefile   = strrep(wholefile,oldtbname,newtbname);
 
 % write it over again
-writetbjsonfile(jspath,wholefile);
+try
+   writetbjsonfile(jspath,wholefile);
+end
