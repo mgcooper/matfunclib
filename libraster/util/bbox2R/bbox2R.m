8ad978396159d88e2a63ae49c5888ccd919cc149
mgcooper <mguycooper@gmail.com>
2023-06-28 09:32:51 -0700

    fix(bbox2R): fix R object parsing, add projcrs optional input

--- libraster/util/bbox2R/bbox2R.m
+++ libraster/util/bbox2R/bbox2R.m
@@ -1,120 +1,204 @@
-function R1 = bbox2R(bbox,gridRes)
-
-%BBOX2R R = bbox2R(bbox,grid_res) constructs spatial referencing object R
-%from 2-by-2 bounding box bbox, for example as provided by mapbbox. The
-%cell spacing set by gridRes must be constant. If necessary, maprefcells
-%adjusts the limits of the raster slightly to ensure an integer number of
-%cells in each dimension.
-
+function R = bbox2R(bbox, gridRes, varargin)
+%BBOX2R construct spatial referencing object R from 2-by-2 bounding box bbox
+%
+% R = bbox2R(bbox,grid_res) constructs spatial referencing object R from 2-by-2
+% bounding box bbox, for example as provided by mapbbox. The cell spacing set by
+% gridRes must be constant. If necessary, maprefcells adjusts the limits of the
+% raster slightly to ensure an integer number of cells in each dimension.
+%
 %   Author: Matt Cooper, guycooper@ucla.edu, June 2019
 %   Citation: Matthew Cooper (2019). matrasterlib
 %   (https://www.github.com/mguycooper/matrasterlib), GitHub. Retrieved MMM
 %   DD, YYYY.
-
+%
 %   Syntax
-
+%
 %   R = bbox2R(bbox,gridRes)
-
+%
 %   Description
-
-%   R = bbox2R(bbox,gridRes) constructs spatial referencing object R from
+%
+%   R = bbox2R(bbox, gridRes) constructs spatial referencing object R from
 %   2-by-2 bounding box BBOX. BBOX bounds the outer edges of the image in
 %   map coordinates:
 %
 %                           [minX minY
 %                            maxX maxY]
-
-%   parameter gridRes sets the grid spacing. 
-
-%   See also mapbbox
-
+%
+%   parameter gridRes sets the grid spacing.
+%
 %   Examples
-
+%
 % % Example 1: verify function accuracy against in-built functions
 % %   load a raster Z and its referencing object R
-%     [Z,R1]  = readgeoraster('MtWashington-ft.grd','OutputType','double');
-% %   take the bounding box 
-%     bbox    = mapbbox(R1,R1.RasterSize(1),R1.RasterSize(2));
-%     R2      = bbox2R(bbox,R1.CellExtentInWorldY);
-%     Rcheck  = [R1;R2];
+%     [~,R1] = readgeoraster('MtWashington-ft.grd');
+% %   take the bounding box
+%     bbox = mapbbox(R1,R1.RasterSize(1),R1.RasterSize(2));
+%     R2 = bbox2R(bbox,R1.CellExtentInWorldY, R1.ProjectedCRS.wktstring);
 % %   examine Rcheck to confirm the function works
-    
+%     isequal(R1, R2)
+%
 % % Example 2: build a custom bbox and georeference it
 % %   load a raster Z and its referencing object R
-%     [Z,R]   = readgeoraster('concord_ortho_w.tif');
+%     [Z, R] = readgeoraster('concord_ortho_w.tif');
 % %   load a shapefile, and make a bounding box around it
-%     pond    = shaperead('concord_hydro_area.shp', 'RecordNumbers', 14);
+%     pond = shaperead('concord_hydro_area.shp', 'RecordNumbers', 14);
 % %   query the ortho intensity values within the area defined by pond
-%     bbox    = pond.BoundingBox;
-%     Rbox    = bbox2R(bbox,10);
+%     bbox = pond.BoundingBox;
+%     Rbox = bbox2R(bbox, 10);
 %     [xq,yq] = R2grid(Rbox);
-%     I       = mapinterp(Z,R,xq,yq,'nearest');
+%     I = mapinterp(Z,R,xq,yq,'nearest');
 %     mean(I(:)) % mean intensity value inside 'pond'
-    
-    
+% %   plot the original and downsampled images
+%     figure; mapshow(Z, R);
+%     figure; mapshow(I, Rbox);
+% 
+%   See also mapbbox
+
 %% Check inputs
 
 % confirm mapping toolbox is installed
 assert(license('test','map_toolbox')==1, ...
-                        'bb2R requires Matlab''s Mapping Toolbox.')
+   'bb2R requires Matlab''s Mapping Toolbox.')
 
-% confirm there is one inputs
-narginchk(1,2)
+% confirm there is one input
+narginchk(1,4)
 
 % confirm BBOX is a 2-by-2 numeric grid
-validateattributes(bbox, {'numeric'}, {'2d','size',[2,2]}, 'bb2R', 'bbox', 1)
-validateattributes(gridRes, {'numeric'}, {'scalar'}, 'bb2R', 'gridRes', 2)
+validateattributes(bbox, {'numeric'}, {'2d','size',[2,2]}, 'bbox2R', 'bbox', 1)
+validateattributes(gridRes, {'numeric'}, {'scalar'}, 'bbox2R', 'gridRes', 2)
 
 % bounding box edges
-minX    =   bbox(1,1);
-maxX    =   bbox(2,1);
-minY    =   bbox(1,2);
-maxY    =   bbox(2,2);
-ylims   =   double([floor(minY) ceil(maxY)]);
-xlims   =   double([floor(minX) ceil(maxX)]);
+minX = bbox(1,1);
+maxX = bbox(2,1);
+minY = bbox(1,2);
+maxY = bbox(2,2);
+ylims = double([floor(minY) ceil(maxY)]);
+xlims = double([floor(minX) ceil(maxX)]);
 
 % set the grid spacing
-xres    =   gridRes;
-yres    =   gridRes;
+xres = gridRes;
+yres = gridRes;
 
-% determine if the data is planar or geographic and call the appropriate function
-tf      =   islatlon(minY,minX);
-if tf == 1
-    R1  =   geobox2R(xlims,ylims,xres,yres);
+% determine if the data is planar or geographic
+tfgeo = islatlon(minY,minX);
+
+% if provided, parse the map projection
+if nargin == 3
+
+   mapProj = varargin{1};
+   validateattributes(mapProj, {'projcrs', 'geocrs', 'char', 'string'}, ...
+      {'nonempty'}, 'bbox2R', 'mapProj', 3)   
+   mapProj = parseMapProjection(tfgeo, mapProj);
+
+elseif nargin == 4
+
+   projCode = varargin{1};
+   projAuth = varargin{2};
+   validateattributes(projCode, {'numeric'}, {'nonempty'}, ...
+      'bbox2R', 'projCode', 3)
+   validateattributes(projAuth, {'char', 'string'}, {'nonempty'}, ...
+      'bbox2R', 'projAuth', 4)
+   mapProj = parseMapProjection(tfgeo, projCode, projAuth);
+   
 else
-    R1  =   mapbox2R(xlims,ylims,xres,yres);
+   mapProj = [];
+end
+
+% call the appropriate function
+if tfgeo == 1
+   R = geobox2R(xlims,ylims,xres,yres,mapProj);
+else
+   R = mapbox2R(xlims,ylims,xres,yres,mapProj);
+end
+
 end
 
 %% apply the appropriate function
 
-    function R = mapbox2R(xlims,ylims,xres,yres)
-        
-        % build R
-        R   =   maprefcells(    xlims,ylims,xres,yres,      ...
-                                'ColumnsStartFrom','north', ...
-                                'RowsStartFrom', 'west');
-    end
-
-    function R = geobox2R(xlims,ylims,xres,yres)
-        
-        % 'columnstartfrom','south' is default and corresponds to N-S
-        % oriented grid as in index (1,1) is NW corner
-        R   =   georefcells(    ylims,xlims,xres,yres, ...
-                                'ColumnsStartFrom','north', ...
-                                'RowsStartFrom', 'west');
-    end
-        
+function R = mapbox2R(xlims,ylims,xres,yres,mapProj)
+% build R
+R = maprefcells(xlims,ylims,xres,yres, ...
+   'ColumnsStartFrom','north', ...
+   'RowsStartFrom', 'west');
+R.ProjectedCRS = mapProj;
 end
 
-% Notes 
 
-% for reference, the N-S E-W could be handled here but the error message 
+function R = geobox2R(xlims,ylims,xres,yres,mapProj)
+
+% 'columnstartfrom','south' is default and corresponds to N-S
+% oriented grid as in index (1,1) is NW corner
+R = georefcells(ylims,xlims,xres,yres, ...
+   'ColumnsStartFrom','north', ...
+   'RowsStartFrom', 'west');
+R.GeographicCRS = mapProj;
+end
+
+
+function mapProj = parseMapProjection(tfgeo, varargin)
+
+if nargin == 2
+
+   mapProj = varargin{1};
+
+   % if mapProj is a string or char, assume it is a wkt string
+   if isa(mapProj, 'char') || isa(mapProj, 'string')
+      if tfgeo
+         try
+            mapProj = geocrs(mapProj);
+         catch ME
+            rethrow(ME);
+         end
+      else
+         try
+            mapProj = projcrs(mapProj);
+         catch ME
+            rethrow(ME);
+         end
+      end
+   else
+      % mapProj is already a projcrs or geocrs
+   end
+
+elseif nargin == 3
+
+   projCode = varargin{1};
+   projAuth = varargin{2};
+   
+   % Try to create the map projection
+   if tfgeo
+      try
+         mapProj = geocrs(projCode, "Authority", projAuth);
+      catch ME
+         rethrow(ME);
+      end
+   else
+      try
+         mapProj = projcrs(projCode, "Authority", projAuth);
+      catch ME
+         rethrow(ME);
+      end
+   end
+end
+
+% mapProj is a projcrs or geocrs, confirm it matches tfgeo
+if tfgeo
+   assert(isa(mapProj, 'geocrs'), 'Inconsistent coordinate system')
+else
+   assert(isa(mapProj, 'projcrs'), 'Inconsistent coordinate system')
+end
+
+end
+
+% Notes
+
+% for reference, the N-S E-W could be handled here but the error message
 % is less informative than custom message with assert
 % % confirm X and Y are 2d numeric grids of equal size oriented N-S and E-W
-% validateattributes(X(1,:),  {'numeric'}, ... 
-%                             {'2d','size',size(Y(1,:)),'increasing'}, ... 
-%                             'R2grid', 'X', 1)
+% validateattributes(X(1,:),  {'numeric'}, ...
+%    {'2d','size',size(Y(1,:)),'increasing'}, ...
+%    'R2grid', 'X', 1)
 % validateattributes(Y(1,:),  {'numeric'}, ...
-%                             {'2d','size',size(X(1,:)),'decreasing'}, ...
-%                             'R2grid', 'Y', 2)
+%    {'2d','size',size(X(1,:)),'decreasing'}, ...
+%    'R2grid', 'Y', 2)
 
