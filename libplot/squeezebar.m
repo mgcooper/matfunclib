29ac740da2432086d0b9afc569297584b253a340
mgcooper <mguycooper@gmail.com>
2023-07-11 13:24:04 -0700

    chore: remove unused functions

--- libplot/squeezebar.m
+++
@@ -1,4 +0,0 @@
-function h = squeezebar( data,varargin )
-%SQUEEZEBAR Summary of this function goes here
-h = bar(squeeze(data),varargin{:});
-
--- libplot/squeezeplot.m
+++
@@ -1,4 +0,0 @@
-function h = squeezeplot( data,varargin )
-%SQUEEZEPLOT Summary of this function goes here
-h = plot(squeeze(data),varargin{:});
-
