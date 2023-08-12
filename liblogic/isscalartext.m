2f576091ffdc15b13d8cc63cd494fadc370c3ace
mgcooper <mguycooper@gmail.com>
2023-08-11 23:35:21 -0700

    feat(isscalartext): add function to validate scalar text

---
+++ liblogic/isscalartext.m
@@ -0,0 +1,81 @@
+function tf = isscalartext(X)
+%ISSCALARTEXT general description of function
+%
+%  TF = ISSCALARTEXT(X) returns TF = true if X is a row char or a scalar string
+%
+% Examples
+% tf = isscalartext('')
+% tf =
+% logical
+%  1
+% 
+% tf = isscalartext('scalar')
+% tf =
+% logical
+%  1
+% 
+% tf = isscalartext("scalar")
+% tf =
+% logical
+%  1
+%
+% tf = isscalartext(["scalar", "nonscalar"])
+% tf =
+% logical
+%  0
+%
+% tf = isscalartext({'scalar'})
+% tf =
+% logical
+%  0
+% 
+% Copyright (c) 2023, Matt Cooper, BSD 3-Clause License, www.github.com/mgcooper
+%
+% See also: ischarlike
+
+% PARSE INPUTS
+narginchk(1,1)
+
+% MAIN CODE
+tf = ( ischar(X) && (isrow(X) || isequal(X, '')) ) || ...
+   (isstring(X) && isscalar(X));
+
+end
+
+%% TESTS
+
+%!test
+
+% ## add octave tests here
+
+%% LICENSE
+
+% BSD 3-Clause License
+%
+% Copyright (c) YYYY, Matt Cooper (mgcooper)
+% All rights reserved.
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+%
+% 1. Redistributions of source code must retain the above copyright notice, this
+%    list of conditions and the following disclaimer.
+%
+% 2. Redistributions in binary form must reproduce the above copyright notice,
+%    this list of conditions and the following disclaimer in the documentation
+%    and/or other materials provided with the distribution.
+%
+% 3. Neither the name of the copyright holder nor the names of its
+%    contributors may be used to endorse or promote products derived from
+%    this software without specific prior written permission.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+% DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+% FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+% DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+% SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+% CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+% OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+% OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
---
+++ liblogic/test/test_isscalartext.m
@@ -0,0 +1,155 @@
+%TEST_ISSCALARTEXT test isscalartext
+
+% Define test data
+S = struct('field1', 'value1', 'field2', 'value2');
+
+%% Test function accuracy
+
+% Test function accuracy using assert
+expected = [];
+returned = isscalartext( );
+assert(isequal(returned, expected));
+
+% Test function accuracy using assert with tolerance
+tol = 20*eps;
+assert(abs(returned - expected) < tol);
+
+% Test with edge cases (Inf, NaN, very large/small numbers)
+assert(isnan(isscalartext(NaN)));
+assert(isinf(isscalartext(Inf)));
+assert(abs(isscalartext(1e200) - 1e200) < 1e-10); % replace with theoretical result
+
+%% Test error handling
+
+% This uses the custom local functions to mimic the testsuite features like
+% verifyError in a script-based testing framework. See the try-catch versions
+% further down for another way to test error handling.
+
+testdiag = "description of this test";
+eid = 'matfunclib:libname:isscalartext';
+
+% verify failure
+assertError(@() isscalartext('inputs that error'), eid, testdiag)
+
+% verify success
+assertSuccess(@() isscalartext('inputs that dont error'), eid, testdiag)
+
+%% Test type handling
+
+% Test different types, if the function is expected to handle them
+
+expected = []; % add expected value for double type
+assert(isequal(isscalartext(X), expected));
+
+expected = []; % add expected value for single type
+assert(isequal(isscalartext(single(X)), single(expected)));
+
+expected = []; % add expected value for logical type
+assert(isequal(isscalartext(logical(X)), expected));
+
+expected = []; % add expected value for int16 type
+assert(isequal(isscalartext(int16(X)), expected)); % int16
+
+%% Test dimension handling
+
+% Test different dimensions
+assert(isequal(isscalartext([2 3]), [4 6])); % 1D array
+assert(isequal(isscalartext([2 3; 4 5]), [4 6; 8 10])); % 2D array
+
+%% Test empty inputs
+try
+   isscalartext();
+   error('Expected an error for empty inputs, but none was thrown');
+catch ME
+   assert(strcmp(ME.identifier, 'MATLAB:minrhs'));
+end
+
+%% Test invalid inputs
+try
+   isscalartext('invalid');
+   error('Expected an error for invalid inputs, but none was thrown');
+catch ME
+   % if arguments block is used:
+   assert(strcmp(ME.identifier, 'MATLAB:validation:UnableToConvert'));
+   % otherwise, might be:
+   % assert(strcmp(ME.identifier, 'MATLAB:invalidInput'));
+end
+
+%% Test too many input arguments
+try
+   isscalartext(1,2,3,4,5,6,7);
+   error('Expected an error for too many inputs, but none was thrown');
+catch ME
+   assert(strcmp(ME.identifier, 'MATLAB:TooManyInputs'));
+end
+
+
+% -------------------------------------
+% LOCAL FUNCTIONS
+% -------------------------------------
+function assertError(fh, eid, varargin)
+%ASSERTERROR assert error using function handle and error id
+
+   import matlab.unittest.diagnostics.Diagnostic;
+   import matlab.unittest.constraints.Throws;
+   
+   throws = Throws(eid);
+   passed = throws.satisfiedBy(fh);
+   diagText = ""; % set empty string for passed == true
+   if ~passed
+       diag = Diagnostic.join(varargin{:}, throws.getDiagnosticFor(fh));
+       arrayfun(@diagnose, diag);
+       diagText = strjoin({diag.DiagnosticText},[newline newline]);
+   end
+   assert(passed, diagText); 
+end
+
+function assertSuccess(fnc, eid, varargin)
+%ASSERTSUCCESS assert success using function handle and error id
+
+   import matlab.unittest.diagnostics.Diagnostic;
+   import matlab.unittest.constraints.Throws;
+   
+   throws = Throws(eid);
+   passed = throws.satisfiedBy(fnc);
+   diagText = ""; % set empty string for passed == true
+   if passed
+       diag = Diagnostic.join(varargin{:}, throws.getDiagnosticFor(fnc));
+       arrayfun(@diagnose, diag);
+       diagText = strjoin({diag.DiagnosticText},[newline newline]);
+   end
+   assert(~passed, diagText); 
+end
+
+function assertWithAbsTol(returned, expected, tol, varargin)
+%ASSERTWITHABSTOL assert equality within an absolute tolerance.
+% 
+% assertWithAbsTol(returned, expected) uses tol = 1e-6
+% assertWithAbsTol(returned, expected, tol)
+% assertWithAbsTol(returned, expected, tol, msg)
+% 
+% Helper function to assert equality within an absolute tolerance.
+% Takes two values and an optional message and compares
+% them within an absolute tolerance of 1e-6.
+
+if nargin < 4 || isempty(tol), tol = 1e-6; end
+   
+   tf = abs(returned-expected) <= tol;
+   assert(tf, varargin{:});
+end
+
+function assertWithRelTol(returned, expected, relTol, varargin)
+%ASSERTWITHABSTOL assert equality within a relative tolerance.
+% 
+%  assertWithRelTol(returned, expected) uses relTol = 0.1%
+%  assertWithRelTol(returned, expected, relTol)
+%  assertWithRelTol(returned, expected, relTol, msg)
+% 
+% Helper function to assert equality within a relative tolerance.
+% Takes two values and an optional message and compares
+% them within a relative tolerance of 0.1%.
+   if nargin < 4 || isempty(relTol), relTol = 0.001; end
+      
+   tf = abs(expected - returned) <= relTol.*abs(expected);
+   assert(tf, varargin{:});
+end
