9b819973d29fd369ccefcc9f987b8e67c4b37be3
mgcooper <mguycooper@gmail.com>
2023-09-07 16:14:04 -0700

    feat: add option to plot Z with default X,Y

--- libraster/util/plotraster.m
+++ libraster/util/plotraster.m
@@ -1,14 +1,14 @@
 function varargout = plotraster(Z, varargin)
 %PLOTRASTER
-% 
+%
 % plotraster(Z, R)
-% 
+%
 % plotraster(Z, X, Y)
-% 
+%
 % See also: rastershow, mapshow, geoshow, imagesc, imagscn
 
 % Verify the input count
-narginchk(2, Inf)
+narginchk(1, Inf)
 
 % Parse possible axes input.
 [ax, args, nargs] = axescheck(varargin{:}); %#ok<ASGLU>
@@ -17,15 +17,21 @@ narginchk(2, Inf)
 if isempty(ax)
    ax = gca;
 end
-    
+
 % Parse the inputs. Compute the X and Y coordinates of the centers of the first
 % and last columns and first and last rows.
-if nargs == 1
+if nargs == 0
+   X = 1:size(Z, 2);
+   Y = size(Z, 1):-1:1;
+   XTicks = X;
+   YTicks = Y;
+   
+elseif nargs == 1
    R = args{1};
    rasterSize = R.RasterSize;
-   
+
    if R.CoordinateSystemType == "planar"
-      
+
       [X, Y] = R.intrinsicToWorld([1, R.RasterSize(1)], [1, R.RasterSize(2)]);
       XTicks = sort(min(X(:)):R.CellExtentInWorldX:max(X(:)), 'ascend');
       YTicks = sort(min(Y(:)):R.CellExtentInWorldY:max(Y(:)), 'descend');
@@ -35,7 +41,7 @@ if nargs == 1
       % X = [min(X(:)), max(X(:))];
       % Y = [max(Y(:)), min(Y(:))];
    else
-      
+
       X = R.intrinsicXToLongitude([1 rasterSize(2)]);
       Y = R.intrinsicYToLatitude( [1 rasterSize(1)]);
 
@@ -46,7 +52,7 @@ elseif nargs == 2
    [XTicks, YTicks] = gridvec(varargin{1}, varargin{2});
    X = [min(XTicks(:)) max(XTicks(:))];
    Y = [max(YTicks(:)) min(YTicks(:))];
-   
+
    %Y = [min(Y(:)) max(Y(:))];
 end
 
@@ -57,13 +63,13 @@ YTicks = sort(YTicks(1:round(numel(YTicks)/numticks):end));
 
 % X and Y are two-element vectors representing the coordinates of the first and
 % last pixel centers. X(1), Y(1) = the upper left pixel, X(2), Y(2) = the lower
-% right pixel 
+% right pixel
 H = image(Z, 'XData', X, 'YData', Y, 'CDataMapping', 'scaled', 'Parent', ax);
 
 % For reference, this would work too, but X, Y might need to be the coordinates
 % H = imagesc(X, Y, Z);
-% set(gca,'YDir','normal'); 
-            
+% set(gca,'YDir','normal');
+
 % Override the automatic reversal of YDir by image.
 set(ax, 'Ydir', 'Normal', 'XTick', XTicks, 'YTick', YTicks, 'TickLength', [0 0]);
 grid off
@@ -86,7 +92,7 @@ end
 % if ~isempty(props)
 %   set(H,props{:,1},props{:,2});
 % end
-% 
+%
 % % Set labels if necessary
 % if ~isempty(xlbl)
 %   xlabel(xlbl);
@@ -94,7 +100,7 @@ end
 % if ~isempty(ylbl)
 %   ylabel(ylbl);
 % end
-% 
+%
 % % Return the handle
 % if nargout == 1
 %   H = H;
