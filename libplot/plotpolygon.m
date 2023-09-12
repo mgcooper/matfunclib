b109a147f9508fb69f0849d50b58e367b1667ff9
mgcooper <mguycooper@gmail.com>
2023-09-11 17:22:19 -0700

    feat(plotpolygon): Add metaclass inheritance

--- libplot/plotpolygon.m
+++ libplot/plotpolygon.m
@@ -1,17 +1,23 @@
-function varargout = plotpolygon(P)
-%PLOTPOLYGON plot polygon data
+function varargout = plotpolygon(P, PlotOpts)
+   %PLOTPOLYGON plot polygon data
+   arguments
+      P
+      PlotOpts.?matlab.graphics.primitive.Polygon
+   end
+   PlotOpts = namedargs2cell(PlotOpts);
+   
+   if isa(P,'polyshape')
+      H = arrayfun(@(n) plot(P(n), PlotOpts{:}), 1:numel(P));
+   elseif iscell(P)
+      H = cellfun(@(x, y) plot(x, y, PlotOpts{:}), P(:, 1), P(:, 2));
+   elseif ismatrix(P)
+      H = plot(P(:, 1), P(:, 2), PlotOpts{:});
+   end
 
-if isa(P,'polyshape')
-   H = arrayfun(@(n) plot(P(n)),1:numel(P));
-elseif iscell(P)
-   H = cellfun(@(x,y) plot(x,y),P(:,1),P(:,2));
-elseif ismatrix(P)
-   H = plot(P(:,1),P(:,2));
-end
+   % For reference, if PX,PY cell arrays are passed in:
+   % cellfun(@plot, PX, PY);
 
-% For reference, if PX,PY cell arrays are passed in:
-% cellfun(@plot, PX, PY);
-
-if nargout == 1
-   varargout{1} = H;
+   if nargout == 1
+      varargout{1} = H;
+   end
 end
