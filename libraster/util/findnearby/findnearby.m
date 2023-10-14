041baa80e159d2d0e5bad937e5acdccd8c9e05be
mgcooper <mguycooper@gmail.com>
2023-10-13 22:17:35 -0700

    refactor(findnearby)!: Handle 2d array input.
    Notes say this likely breaks findnearby.
    
    ncrowcol has new "debug" notes and recollection is it might have broken too.

--- libraster/util/findnearby/findnearby.m
+++ libraster/util/findnearby/findnearby.m
@@ -1,61 +1,81 @@
-function [row,col,distance,idx] = findnearby(x,y,xq,yq,N)
-%function [row,col,dist] = findnearby(x,y,xq,yq,N)
-%
-%FINDNEARBY finds N nearest points to xq,yq within X,Y. Equivalent to
-%dsearchn for N=1. Locates a set of "nearby" points, rather than the
-%"nearest" points, which is what dsearchn returns.
-%
-% Usage:
-% [row,col] = findnearby(x,y,xq,yq,N)
-% idx       = findnearby(x,y,xq,yq,N)
-% [-,dist]  = findnearby(x,y,xq,yq,N) % use with any previous
-% 
-% Matt Cooper @(c) 2021
-% 
-% See also
+function varargout = findnearby(x,y,xq,yq,N)
+   %FINDNEARBY find N nearest points in X,Y to M coordinate pairs Xq,Yq
+   %
+   % [ROW, COL] = findnearby(X,Y,Xq,Yq,N) returns NxM arrays ROW and COL, of row
+   % and column indices for N coordinate pairs in X,Y nearest the M coordinate
+   % pairs in Xq, Yq in euclidean space.
+   %
+   % [ROW, COL, DST] = findnearby(X,Y,Xq,Yq,N) also returns DST, an NxM array of
+   % distances between each query point and its nearest point.
+   %
+   % [ROW, COL, DST, IDX] = findnearby(X,Y,Xq,Yq,N) also returns IDX, an NxM
+   % array of linear indices for each nearest point in X,Y to each query point
+   % in Xq,Yq.
+   %
+   % IDX = findnearby(X,Y,Xq,Yq,N) returns the linear index.
+   %
+   % Example
+   % Xq, Yq are each 3x1 vectors representing 3 x-y coordinate pairs, then M =
+   % 3. To find the 5 nearest points in X,Y to each of the 3 coordinate pairs in
+   % Xq, Yq, set N = 5. The outputs ROW and COL will be 3x5 arrays representing
+   % the indices in X,Y for the 5 nearest points in X,Y to the 3 query points in
+   % Xq,Yq.
+   %
+   %
+   % FINDNEARBY uses dsearchn to locate a set of "nearby" points, rather than
+   % the "nearest" point, which is what dsearchn returns.
+   %
+   %
+   % Matt Cooper @(c) 2021
+   %
+   % See also: dsearchn
 
-if nargin == 4 || isempty(N)
-   N = 1;
+   % I might have broken this by adding multiple points b/c the found thing
+   % might remove them before they are found nearest the others
+
+   if nargin == 4 || isempty(N)
+      N = 1;
+   end
+
+   % Keep the original x,y array shapes, then reshape to column vectors
+   wasvec = isvector(x) && isvector(y);
+   sizexy = size(x);
+   x = x(:);
+   y = y(:);
+   xq = xq(:);
+   yq = yq(:);
+
+   % Initialize the outputs
+   M = numel(xq);
+   row = nan(M, N);
+   col = nan(M, N);
+   idx = nan(M, N);
+   dst = nan(M, N);
+
+   % Use to exclude found points
+   found = false(size(x));
+
+   % find N requested nearby points
+   n = 0;
+   while n<N
+      n = n+1;
+
+      [idx(:, n), dst(:, n)] = dsearchn([x(~found) y(~found)],[xq yq]);
+      [row(:, n), col(:, n)] = ind2sub(sizexy,idx(:, n));
+      found(idx(:, n)) = true;
+   end
+
+   if wasvec
+      assert(all(col(:) == 1))
+   end
+   % row = reshape(row, sizexy);
+   % col = reshape(col, sizexy);
+
+   % package output
+   nargoutchk(1, 4)
+   if nargout == 1
+      varargout{1} = idx;
+   else
+      [varargout{1:nargout}] = dealout(row, col, dst, idx);
+   end
 end
-
-row = nan(N,1);
-col = nan(N,1);
-idx = nan(N,1);
-dst = nan(N,1);
-
-% keep the original size of the x,y coordinate arrays
-sizexy = size(x);
-x = x(:);
-y = y(:);
-
-% we will exclude found points
-found = false(size(x));
-
-% find N requested nearby points
-n = 0;
-while n<N
-
-   n = n+1;
-
-   [idx(n),dst(n)] = dsearchn([x(~found) y(~found)],[xq yq]);
-   [row(n),col(n)] = ind2sub(sizexy,idx(n));
-
-   % exclude the found point and repeat the search
-   found(idx(n)) = true;
-end
-
-distance = dst;
-
-%     % package output
-%     switch nargout
-%         case 1
-%             varargout       = idx;
-%         case 2 % note: could be [idx, dist]
-%             varargout{1}    = row;
-%             varargout{2}    = col;
-%         case 3
-%             varargout{1}    = row;
-%             varargout{2}    = col;
-%             varargout{3}    = dist;
-%     end
-
--- libraster/util/ncrowcol/ncrowcol.m
+++ libraster/util/ncrowcol/ncrowcol.m
@@ -14,19 +14,17 @@ function [start, count] = ncrowcol(ncvar, ncX, ncY, xpoly, ypoly, varargin)
 % workspace, but I want it to accept the name of a variable within the
 % nc file so it can query that file to determine the
 
-varchar = false;    % assume a data var (not char) was provided
+varIsChar = false; % assume a data var was provided rather than char variable name
 if ischar(ncvar)
-   info    = varargin{1};
-   ivar    = find(ismember(info.Name,ncvar));
+   info = varargin{1};
+   ivar = ismember(info.Name,ncvar);
    varsize = info.Size{ivar};
-   varchar = true;
+   varIsChar = true;
 end
 
-%     % check if input is geographic or projected
-%     if islatlon(ncY,ncX)
-%     end
-
-% in order to be able to pass in poly and
+% % check if input is geographic or projected
+% if islatlon(ncY,ncX)
+% end
 
 % check if polygon is provided or just a point
 if numel(xpoly) > 1
@@ -46,23 +44,27 @@ end
 % NOTE: based on ncrowcol_mar, it might be necessary to use unique(r)
 % and then
 
+% previously I used this for the count, but it is wrong when there is a missing
+% value in the rows or columns i.e. when all(diff(r) == 1) is not true
+rowcount = @(r) max(r)-min(r)+1; % rowcount = @(r) length(unique(r));
+colcount = @(c) max(c)-min(c)+1; % colcount = @(c) length(unique(c));
 
-if varchar == false
+if varIsChar == false
    if iscolumn(ncvar)
       start = r(1);
-      count = length(unique(r));
+      count = colcount(c);
    elseif isrow(ncvar)
       start = c(1);
-      count = length(unique(c));
+      count = colcount(c);
    elseif ismatrix(ncvar)
       start = [r(1),c(1)];
-      count = [length(unique(r)),length(unique(c))];
+      count = [rowcount(r), colcount(c)];
    elseif ndims(ncvar)==3
       start = [r(1),c(1),1];
-      count = [length(unique(r)),length(unique(c)),size(ncvar,3)];
+      count = [rowcount(r),colcount(c),size(ncvar,3)];
    elseif ndims(ncvar)==4
       start = [r(1),c(1),1,1];
-      count = [length(unique(r)),length(unique(c)),size(ncvar,3),size(ncvar,4)];
+      count = [rowcount(r),colcount(c),size(ncvar,3),size(ncvar,4)];
    else
       warning('This function does not support 4-d or higher dimension variables');
    end
@@ -73,16 +75,16 @@ else
 
    if numel(varsize) == 1
       start = r(1);
-      count = length(unique(r));
+      count = rowcount(r);
    elseif numel(varsize) == 2
       start = [r(1),c(1)];
-      count = [length(unique(r)),length(unique(c))];
+      count = [rowcount(r),colcount(c)];
    elseif numel(varsize) == 3
       start = [r(1),c(1),1];
-      count = [length(unique(r)),length(unique(c)),varsize(3)];
+      count = [rowcount(r),colcount(c),varsize(3)];
    elseif numel(varsize) == 4
       start = [r(1),c(1),1,1];
-      count = [length(unique(r)),length(unique(c)),varsize(3),varsize(4)];
+      count = [rowcount(r),colcount(c),varsize(3),varsize(4)];
    else
       warning('This function does not support 4-d or higher dimension variables');
    end
@@ -102,5 +104,28 @@ end
 %    'If the third dimension is time, then setting it to 1 ' ...
 %    'would mean '};
 
+
+% DEBUGGING - this was in the if numel(xpoly) > 1 section, it came up when I got
+% fewer x,y points when using start,count from this function than I did when I
+% read in the full lat, lon grids from the .nc file and used inpolygon to get
+% the points inside the polygon. The real issue was that the polygon needs to be
+% buffered (lesson learned in interpolationPoints), but fortunately it also
+% inadvertently revealed that if the r, c points found somehow miss a value for
+% example normally rows might be 1,2,3,4 but sometimes it skips a value e.g.
+% 1,3,4, in which case 
+
+% r,c and R,C, contain the same unique points
+% [R, C] = fullgrid(r, c);
+% figure;
+% scatter(R(:), C(:), 'filled'); hold on;
+% scatter(r(:), c(:), 'filled');
+
+% the x,y points plotted here cover more of the polygon than the x,y points
+% extracted from the nc file using start, count, so something must be going
+% wrong ... figured it out, see notes above.
+% figure;
+% scatter(ncX(inpoly), ncY(inpoly), 'filled'); hold on;
+% plot(xpoly, ypoly)
+   
 end
 
