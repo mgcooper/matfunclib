9bfc0bec9b754f7b33e9f8e579078d73a2c41beb
mgcooper <mguycooper@gmail.com>
2023-10-27 00:05:02 -0700

    refactor(geomap): Add nargin==0 option to create default world map and optional buffer.

--- libspatial/geomap.m
+++ libspatial/geomap.m
@@ -1,15 +1,35 @@
 function varargout = geomap(lat,lon,varargin)
    %GEOMAP Initialize a worldmap in geographic coordinates
    %
-   %  ax = geomap(lat,lon) creates a worldmap with limits = bounds(lat),
-   %  bounds(lon)
+   %  ax = geomap(lat, lon) creates a worldmap with
+   %  limits = bounds(lat), bounds(lon)
    %
    % See also: getgcm
 
-   [latmin,latmax] = bounds(lat);
-   [lonmin,lonmax] = bounds(lon);
+   if nargin == 0
+      ax = worldmap([-90 90], [-180 180]);
+      return
+   elseif nargin == 3
+      xbuffer = varargin{1};
+      ybuffer = xbuffer;
+   elseif nargin == 4
+      xbuffer = varargin{1};
+      ybuffer = varargin{2};
+   else
+      xbuffer = 0;
+      ybuffer = 0;
+   end
 
-   ax = worldmap([latmin,latmax],[lonmin,lonmax]);
+   [latmin, latmax] = bounds(lat);
+   [lonmin, lonmax] = bounds(lon);
+
+   latmin = latmin - (latmax - latmin) * ybuffer/100;
+   latmax = latmax + (latmax - latmin) * ybuffer/100;
+
+   lonmin = lonmin - (lonmax - lonmin) * xbuffer/100;
+   lonmax = lonmax + (lonmax - lonmin) * xbuffer/100;
+
+   ax = worldmap([latmin, latmax], [lonmin, lonmax]);
 
    switch nargout
       case 1
