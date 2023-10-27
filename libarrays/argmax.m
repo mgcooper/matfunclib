14122d539c9cbcc6d167cfd7d5ad7888d6832496
mgcooper <mguycooper@gmail.com>
2023-10-27 00:03:12 -0700

    feat(argmin/max): Add argmin and argmax functions.

---
+++ libarrays/argmax.m
@@ -0,0 +1,38 @@
+function [index, value] = argmax(varargin)
+   %ARGMAX Index of minimum valued element.
+   %   
+   % I = ARGMAX(X)
+   % I = ARGMAX(X, [], DIM)
+   % I = ARGMAX(X, [], NANFLAG)
+   % I = ARGMAX(X, [], DIM, NANFLAG)
+   % [I, M] = ARGMAX(_)
+   %
+   % I = ARGMAX(X, [], 'all')
+   % I = ARGMAX(X, [], VECDIM)
+   % I = ARGMAX(X, [], 'all', NANFLAG)
+   % I = ARGMAX(X, [], VECDIM, NANFLAG)
+   % [I, M] = ARGMAX(X, [], 'all', _)
+   % 
+   % See documentation for MAX for additional syntax options.
+   %
+   % I = ARGMAX(X) returns the index I of the minimum valued element of X,
+   % such that X(I) == MAX(X(:)). For vectors, ARGMAX(X) is a scalar index.
+   % For matrices, ARGMAX(X) is a row vector containing the indices of the 
+   % minimum valued elements for each column of X, such that X(I) == MAX(X, [], 1).
+   % 
+   % I = ARGMAX(X, DIM) returns the index I of the minimum valued element 
+   % of X along the dimension specified by DIM.
+   % 
+   % ARGMAX accepts the same inputs as the built-in MAX function.
+   % See the documentation for MAX to understand the requirements for each syntax.
+   %
+   % Example:
+   %     X = [2 8 4;
+   %          7 3 9];
+   %     argmax(X, [], 1)
+   %     argmax(X, [], 2)
+   %
+   % See also ARGMAX, MAX, MAX
+   
+   [value, index] = max(varargin{:});
+end
---
+++ libarrays/argmin.m
@@ -0,0 +1,64 @@
+function [index, value] = argmin(varargin)
+   %ARGMIN Index of minimum valued element.
+   %   
+   % I = ARGMIN(X)
+   % I = ARGMIN(X, [], DIM)
+   % I = ARGMIN(X, [], NANFLAG)
+   % I = ARGMIN(X, [], DIM, NANFLAG)
+   % [I, M] = ARGMIN(_)
+   %
+   % I = ARGMIN(X, [], 'all')
+   % I = ARGMIN(X, [], VECDIM)
+   % I = ARGMIN(X, [], 'all', NANFLAG)
+   % I = ARGMIN(X, [], VECDIM, NANFLAG)
+   % [I, M] = ARGMIN(X, [], 'all', _)
+   % 
+   % See documentation for MIN for additional syntax options.
+   %
+   % I = ARGMIN(X) returns the index I of the minimum valued element of X,
+   % such that X(I) == MIN(X(:)). For vectors, ARGMIN(X) is a scalar index.
+   % For matrices, ARGMIN(X) is a row vector containing the indices of the 
+   % minimum valued elements for each column of X, such that X(I) == MIN(X, [], 1).
+   % 
+   % I = ARGMIN(X, DIM) returns the index I of the minimum valued element 
+   % of X along the dimension specified by DIM.
+   % 
+   % ARGMIN accepts the same inputs as the built-in MIN function.
+   % See the documentation for MIN to understand the requirements for each syntax.
+   %
+   % Example:
+   %     X = [2 8 4;
+   %          7 3 9];
+   %     argmin(X, [], 1)
+   %     argmin(X, [], 2)
+   %
+   % See also ARGMAX, MIN, MAX
+   
+   [value, index] = min(varargin{:});
+end
+
+%% To not support the MIN (A, B) syntax:
+
+% Add this to the function body:
+
+% secondInputNotEmpty = false;
+% if ~isempty(varargin{2})
+%    varargin = [varargin(1), [], varargin(2:end)];
+% end
+
+% Then remove the [] from the syntax documentation.
+
+%%
+% Started to parse inputs, see wmean for a method that could be converted to
+% a general purpose function for parsing these types of input patterns,
+% common to mean, median, min, max, in some cases w/o the second []
+% comparison input i.e. sort.
+% if nargin == 1
+%    dim = 1;
+% end
+
+% For the simplest case where no matter the input return a scalar, which
+% might be more in keeping with the 'argmin' concept:
+% [value, index] = min(x(:), varargin{:});
+
+% But the user can simply pass in x(:).
