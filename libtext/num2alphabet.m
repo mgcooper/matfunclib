b4d8236f12a71be0488c5b009227d8cb4fef884f
mgcooper <mguycooper@gmail.com>
2023-11-20 16:00:22 -0800

    refactor(fort2mat): Add a few new conversions.

--- libtext/num2alphabet.m
+++ libtext/num2alphabet.m
@@ -36,3 +36,116 @@ function letter = num2alphabet(num)
 
    letter = char(num + 'a' - 1);
 end
+
+% % Found this after writing above:
+% function [let] = num2let(num)
+%    % convert number to sequential letter (ala excel)
+%    % function [let] = num2let(num)
+%    %
+%    % inputs  1
+%    % num     number               class integer
+%    %
+%    % outputs 1
+%    % let     letter designation   class cell
+%    %
+%    % michael arant - oct 16 2013
+%    % michael arant - jan 2016 - slightly faster code and can handle vectors
+%    
+%    if nargin < 1
+%       help num2let; 
+%       error('Need number'); 
+%    end
+%    if isempty(num)
+%       error('Empty number set'); 
+%    end
+%    if ~isint(num)
+%       error('number has to be an integer'); 
+%    end
+%    
+%    % string letters - these are the "base 26" values
+%    str = char(97:122);
+%    
+%    % size output
+%    let = cell(size(num));
+%    
+%    for jj = 1:numel(num)
+%    	% build letter (base 26 equlivant to each value)
+%    	letn = base102basen(num(jj),26);
+%    	% wrap
+%    	if numel(letn) > 1 && ~min(letn(2:end))
+%    		for ii = numel(letn):-1:2
+%    			if letn(ii) < 1
+%    				letn(ii) = letn(ii) + 26; letn(ii-1) = letn(ii-1) - 1;
+%    			end
+%    		end
+%    	end
+%    	if ~letn(1); letn = letn(2:end); end
+%    	% build letters
+%    	let{jj} = str(letn);
+%    end
+% end
+% 
+% %% convert from base 10 to base n (i.e. 26)
+% function [out] = base102basen(num,basen)
+%    % convert base n to base m
+%    % function [out] = base102basen(num,basen)
+%    %
+%    % input  2
+%    % in     number in base m      class real
+%    % basen  output base           class int
+%    %
+%    % output 1
+%    % out    umber in base n       class real
+%    %
+%    % michael arant - oce 16, 2013
+%    
+%    if nargin < 2
+%       help base102basen; 
+%       error('Insuficient inputs'); 
+%    end
+%    
+%    if ~isint(basen)
+%       error('basen needs to be an integer'); 
+%    end
+%    
+%    % convert base10 to basen
+%    out = []; cnt = 0;
+%    while num > 0
+%    	cnt = cnt + 1;
+%    	out(cnt) = mod(num,basen);
+%    	num = floor(num/basen);
+%    end
+%    out = fliplr(out);
+%    % correct decimal
+%    if ~isint(out(end))
+%    	out(end) = floor(out(end)) + mod(out(end),1) * 10 / basen;
+%    end
+% end
+% 
+% %% interget test
+% function [res] = isint(val)
+%    % determines if value is an integer
+%    % function [res] = isint(val)
+%    %
+%    % inputs  1
+%    % val     value to be checked              class real
+%    %
+%    % outputs 1
+%    % res     result (1 is integer, 0 is not)  class integer
+%    %
+%    % michael arant     may 15, 2004
+%    if nargin < 1; help isint; error('I / O error'); end
+%    % numeric?
+%    if ~isnumeric(val); error('Must be numeric'); end
+%    % check for real number
+%    if isreal(val) & isnumeric(val)
+%       %	check for integer
+%    	if round(val) == val
+%    		res = 1;
+%    	else
+%    		res = 0;
+%    	end
+%    else
+%    	res = 0;
+%    end
+% end
