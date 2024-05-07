863c336c257d02d2dfda550fa802746eab16ce6f
mgcooper <mguycooper@gmail.com>
2024-05-07 09:51:00 -0700

    refactor(libhydro)!: Move merra functions to merra project.

--- libhydro/update_merra_filelist.m
+++
@@ -1,8 +0,0 @@
-clean
-
-path2merrafiles = setpath('merra2/arctic','data');
-
-% path2savelist  = setpath('bfra/data','project');
-% merraSaveFilelist(path2merrafiles,path2savelist);
-
-merraSaveFilelist(path2merrafiles);
