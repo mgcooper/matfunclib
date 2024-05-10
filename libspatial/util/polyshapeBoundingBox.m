fdfc272b27fc618e1690c8e241b45920dca9dec4
mgcooper <mguycooper@gmail.com>
2024-05-10 14:38:59 -0700

    feat(polyshapeBoundingBox): Returns polyshape unlike boundingbox.

---
+++ libspatial/util/polyshapeBoundingBox.m
@@ -0,0 +1,26 @@
+function bbox = polyshapeBoundingBox(poly, kwargs)
+   %POLYSHAPEBOUNDINGBOX Bounding box of polyshape, returned as a polyshape.
+   %
+   %  bbox = polyshapeBoundingBox(poly)
+   %
+   % Unlike `boundingbox`, this function returns a polyshape.
+   % 
+   % See also: polyshape, boundingbox
+
+   arguments
+      poly polyshape
+      kwargs.buffer = 0
+   end
+
+   [xbox, ybox] = boundingbox(poly);
+
+   ymin = min(ybox);
+   ymax = max(ybox);
+   xmin = min(xbox);
+   xmax = max(xbox);
+
+   xbox = [xmin xmax xmax xmin xmin];
+   ybox = [ymin ymin ymax ymax ymin];
+
+   bbox = polybuffer(polyshape(xbox, ybox), kwargs.buffer);
+end
