7e06b5ed0a01eecf7efdc3849df6d4110c1a00e9
mgcooper <mguycooper@gmail.com>
2024-07-10 15:51:03 -0700

    feat(replacePackagePrefix): Add method to remove +pkg and pkg. in m-files and json files.

--- templates/toolbox/toolbox/+tbx/+internal/replacePackagePrefix.m
+++ templates/toolbox/toolbox/+tbx/+internal/replacePackagePrefix.m
@@ -15,13 +15,66 @@ function replacePackagePrefix(pathname, old_prefix, new_prefix, varargin)
    % See also:
 
    narginchk(3, 4)
-   if nargin < 4
-      dryrun = true;
-   end
 
    if isoctave
       error([mfilename ' is not octave compatible'])
    end
+
+   [pathname, old_prefix, new_prefix, dryrun] = parseinputs( ...
+      pathname, old_prefix, new_prefix, varargin{:});
+
+   % Create "pkg." and "+pkg" prefixes
+   old_prefix_dot = [old_prefix '.'];
+   new_prefix_dot = [new_prefix '.'];
+
+   plus_old_prefix = ['+' old_prefix];
+   plus_new_prefix = ['+' new_prefix];
+
+   % Generate a list of all mfiles in the project
+   mlist = dir(fullfile(pathname, '**/*.m'));
+   jlist = dir(fullfile(pathname, '**/*.json'));
+
+   list = [mlist; jlist];
+   list(strncmp({list.name}, '.', 1)) = [];
+   list(contains({list.folder}, 'sandbox')) = [];
+
+   % OPEN ALL THE FILES AND REPLACE THE PREFIX
+
+   for n = 1:numel(list)
+
+      filename = fullfile(list(n).folder, list(n).name);
+      fid = fopen(filename);
+
+      % read in the function to a char
+      wholefile = fscanf(fid, '%c');
+      fclose(fid);
+
+      % replace the prefix
+      if contains(wholefile, old_prefix_dot)
+         wholefile = strrep(wholefile, old_prefix_dot, new_prefix_dot);
+      end
+
+      if contains(wholefile, upper(plus_old_prefix))
+         wholefile = strrep(wholefile, upper(plus_old_prefix), upper(plus_new_prefix));
+      end
+
+      if contains(wholefile, lower(plus_old_prefix))
+         wholefile = strrep(wholefile, lower(plus_old_prefix), lower(plus_new_prefix));
+      end
+
+      % REWRITE THE FILE (DANGER ZONE)
+      if dryrun == false
+         fid = fopen(filename, 'wt');
+         fprintf(fid, '%c', wholefile);
+         fclose(fid);
+      else
+         disp(filename)
+      end
+   end
+end
+
+function [pathname, old_prefix, new_prefix, dryrun] = parseinputs( ...
+      pathname, old_prefix, new_prefix, varargin)
 
    parser = inputParser();
    parser.FunctionName = mfilename;
@@ -35,41 +88,19 @@ function replacePackagePrefix(pathname, old_prefix, new_prefix, varargin)
    [pathname, old_prefix, new_prefix] = convertStringsToChars( ...
       pathname, old_prefix, new_prefix);
 
-   % Confirm that the last character of the old/new prefix is a dot
-   if ~endsWith(old_prefix, '.')
-      old_prefix = [old_prefix '.'];
+   % If the old_prefix ends with a dot, remove it (it is appended later)
+   if endsWith(old_prefix, '.')
+      old_prefix = erase(old_prefix, '.');
    end
-   if ~endsWith(new_prefix, '.')
-      new_prefix = [new_prefix '.'];
+   if startsWith(old_prefix, '+')
+      old_prefix = erase(old_prefix, '+');
    end
 
-   % Generate a list of all mfiles in the project
-   list = dir(fullfile(pathname, '**/*.m'));
-   list(strncmp({list.name}, '.', 1)) = [];
-   list(contains({list.folder}, 'sandbox')) = [];
-
-   % OPEN ALL THE FILES AND REPLACE THE PREFIX
-
-   for n = 1:numel(list)
-
-      filename = fullfile(list(n).folder, list(n).name);
-      fid = fopen(filename);
-
-      % read in the function to a char
-      wholefile = fscanf(fid, '%c'); fclose(fid);
-
-      % replace the prefix
-      if contains(wholefile, old_prefix)
-
-         wholefile = strrep(wholefile, old_prefix, new_prefix);
-
-         % REWRITE THE FILE (DANGER ZONE)
-         if dryrun == false
-            fid = fopen(filename, 'wt');
-            fprintf(fid, '%c', wholefile); fclose(fid);
-         else
-            disp(filename)
-         end
-      end
+   % Repeat for the new prefix
+   if endsWith(new_prefix, '.')
+      new_prefix = erase(new_prefix, '.');
+   end
+   if startsWith(new_prefix, '+')
+      new_prefix = erase(new_prefix, '+');
    end
 end
