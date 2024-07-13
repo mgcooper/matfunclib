7aa88cfc47a571996845e104ae1bcb43dd50acfb
mgcooper <mguycooper@gmail.com>
2024-07-13 00:12:12 -0700

    refactor(loadgis): Convert shaperead struct to table then back.
    Move some of the processing applied to m_map_shaperead later so it applies to both shaperead and m_map. Change ambiguous "ok" flag to "used_m_map". Add warnIDs.

--- libdata/loadgis.m
+++ libdata/loadgis.m
@@ -19,39 +19,51 @@ function [S, A] = loadgis(fname, varargin)
    if ~isfile(fname)
       fname = findgisfile(fname);
    end
-   [~,~,ext] = fileparts(fname);
+   [~, ~, ext] = fileparts(fname);
 
    switch ext
       case '.shp'
-         [S,A] = tryshaperead(fname,namedargs,reader);
+         [S, A] = tryshaperead(fname, namedargs, reader);
+
       case '.tif'
          try
-            S = readgeoraster(fname,varargin{:});
+            S = readgeoraster(fname, varargin{:});
          catch ME
+
          end
    end
 end
 
 
-function [S,A] = tryshaperead(fname,namedargs,reader)
+function [S, A] = tryshaperead(fname, namedargs, reader)
+
+   used_m_map = false;
+   has_attrs_tbl = true;
 
    if reader == "m_map"
-      S = m_map_shaperead(fname,namedargs);
-      ok = true;
+      S = m_map_shaperead(fname, namedargs);
+      used_m_map = true;
    else
-      ok = false;
+
       try
-         [S,A] = shaperead(fname,namedargs{:});
+         [S, A] = shaperead(fname, namedargs{:});
          % March 2023, added merge, but it will go to the A section and I think
          % its designed only for m_map, so instead I join the attributes with S
          % here but probably won't work for all geometries
          % ok = true;
-         S = mergestructs(S,A);
+         S = mergestructs(S, A);
+
+         % Wanted to use this to automatically rename X,Y to Lat,Lon for
+         % geostructs, but updateCoordinates is designed to add X,Y or Lat,Lon
+         % coords when the other are already present, given a map projection.
+         % S = updateCoordinates(S)
 
       catch ME
 
-         if strcmp(ME.identifier,'MATLAB:license:checkouterror')
-            warning('Mapping Toolbox checkout error. Using m_shaperead.')
+         if strcmp(ME.identifier, 'MATLAB:license:checkouterror')
+            wid = 'custom:loadgis:MapCheckoutErrorUsingMMap';
+            msg = 'Mapping Toolbox checkout error. Using m_shaperead.';
+            warning(wid, msg)
          end
 
          if strcmp(ME.message,'Unsupported shape type PolyLineZ (type code = 13).')
@@ -59,12 +71,14 @@ function [S,A] = tryshaperead(fname,namedargs,reader)
             % Bounding Rectangle) assumes format [minX  minY  maxX maxY] whereas
             % shaperead BoundingBox is [minX  minY ; maxX maxY]. do the
             % conversion if needed.
-            warning('Requested shapefile is type PolyLineZ. Using m_shaperead.')
+            wid = 'custom:loadgis:PolyLineZUsingMMap';
+            msg = 'Requested shapefile is type PolyLineZ. Using m_shaperead.';
+            warning(wid, msg)
          end
 
          try
-            S = m_map_shaperead(fname,namedargs);
-            ok = true;
+            S = m_map_shaperead(fname, namedargs);
+            used_m_map = true;
          catch ME2
             rethrow(ME2) % throw the error (revisit)
          end
@@ -72,63 +86,73 @@ function [S,A] = tryshaperead(fname,namedargs,reader)
       end
    end
 
-   if ok
-      % the Data produced by m_shaperead may need to be wrangled to a ueseable
-      % format. return to this later. for now, convert the attributes to A and
+   if used_m_map
+      % Data produced by m_shaperead may need to be wrangled to a ueseable
+      % format. return to this later. For now, convert the attributes to A and
       % the lat,lon to S
-      Aok = false;
+      has_attrs_tbl = false;
       try
          A = struct2table(S.dbf); % attribute table
-         Aok = true;
-      catch
+         has_attrs_tbl = true;
+      catch e
+
       end
 
-
-      % note: make sure the lat,lon values that go into elements of S are row vectors
+      % Make sure the lat,lon values that go into elements of S are row vectors
       try
-         %       % if S is scalar (one point, line, polyline, or polygon)
-         %       isscalarstruct = false;
-         %       if numel(S.ncst) == 1 && numel(S.dbf) == 1
-         %          lat = S.ncst{1}(:,2);
-         %          lon = S.ncst{1}(:,1);
-         %          isscalarstruct = true;
-         %       else
-         %          % non-scalar
-         %          lat = cellfun(@transpose,cellfun(@(x)vertcat(x(:,2)),S.ncst,'Uni',0),'Uni',0);
-         %          lon = cellfun(@transpose,cellfun(@(x)vertcat(x(:,1)),S.ncst,'Uni',0),'Uni',0);
-         %       end
+         % if S is scalar (one point, line, polyline, or polygon)
+         % isscalarstruct = false;
+         % if numel(S.ncst) == 1 && numel(S.dbf) == 1
+         %    lat = S.ncst{1}(:,2);
+         %    lon = S.ncst{1}(:,1);
+         %    isscalarstruct = true;
+         % else
+         %    % non-scalar
+         %    lat = cellfun(@transpose, ...
+         %       cellfun(@(x) vertcat(x(:,2)), S.ncst, 'Uniform', 0), 'Uniform', 0);
+         %    lon = cellfun(@transpose, ...
+         %       cellfun(@(x) vertcat(x(:,1)), S.ncst, 'Uniform', 0), 'Uniform', 0);
+         % end
 
-         lat = cellfun(@transpose,cellfun(@(x)vertcat(x(:,2)),S.ncst,'Uni',0),'Uni',0);
-         lon = cellfun(@transpose,cellfun(@(x)vertcat(x(:,1)),S.ncst,'Uni',0),'Uni',0);
+         lat = cellfun(@transpose, ...
+            cellfun(@(x) vertcat(x(:,2)), S.ncst, 'Uniform', 0), 'Uniform', 0);
+         lon = cellfun(@transpose, ...
+            cellfun(@(x) vertcat(x(:,1)), S.ncst, 'Uniform', 0), 'Uniform', 0);
 
-         % for non-scalar, above returns latlon in cell array format, suitable for
+         % for non-scalar, above returns lat lon as cell arrays, suitable for
          % geostruct-type objects and plotting with geoshow. below converts to
          % nan-separated lists, suitable for axesm-based functions like plotm.
 
          % activate this and add it to output for axesm-based compatibility.
-         % [lat,lon] = polyjoin(lat,lon);
+         % [lat, lon] = polyjoin(lat, lon);
 
          switch S.ctype
-            case {'polylineZ','polyline'}
-               T = geostructinit('Line',numel(lat),'fieldnames',S.fieldnames);
+            case {'polylineZ', 'polyline'}
+               GeomType = 'Line';
             case {'polygon'}
-               T = geostructinit('Polygon',numel(lat),'fieldnames',S.fieldnames);
+               GeomType = 'Polygon';
             case {'point'}
-               T = geostructinit('Point',numel(lat),'fieldnames',S.fieldnames);
+               GeomType = 'Point';
+            otherwise
+               GeomType = char.empty;
          end
 
-         %       if isscalarstruct
-         %          T.Lon = lon{:};
-         %          T.Lat = lat{:};
-         % %          % this is if lat/lon are converted to double arrays for the scalar case
-         % %          for n = 1:numel(T)
-         % %             T(n).Lon = lon(n);
-         % %             T(n).Lat = lat(n);
-         % %          end
-         %       else
-         %          [T(1:numel(lon)).Lon] = lon{:};
-         %          [T(1:numel(lat)).Lat] = lat{:};
-         %       end
+         if ~isempty(GeomType)
+            T = geostructinit(GeomType, numel(lat), 'fieldnames', S.fieldnames);
+         end
+
+         % if isscalarstruct
+         %    T.Lon = lon{:};
+         %    T.Lat = lat{:};
+         % % % this is if lat/lon are converted to double arrays for the scalar case
+         % % for n = 1:numel(T)
+         % %    T(n).Lon = lon(n);
+         % %    T(n).Lat = lat(n);
+         % % end
+         % else
+         %    [T(1:numel(lon)).Lon] = lon{:};
+         %    [T(1:numel(lat)).Lat] = lat{:};
+         % end
 
          % this actually works for scalar and non-scalar case if the scalar case is
          % kept as a 1x1 cell, so I commented out the if-else above
@@ -136,178 +160,147 @@ function [S,A] = tryshaperead(fname,namedargs,reader)
          [T(1:numel(lat)).Lat] = lat{:};
 
          % for polygons/lines, get the bounding box of each element
-         if ismember(S.ctype,{'polylineZ','polyline','polygon'})
+         if ismember(S.ctype, {'polylineZ','polyline','polygon'})
             try
                T = updategeostruct(T);
             catch ME3
-               if strcmp(ME3.identifier,'MATLAB:license:checkouterror')
+               if strcmp(ME3.identifier, 'MATLAB:license:checkouterror')
                   T = updateBoundingBox(T);
                end
             end
          end
 
-         if ~isscalar(S)
+         if ~isscalar(T) % jul 2024: was ~isscalar(S), changed to ~isscalar(T)
             T = struct2table(T); % for movevars, also simplifies field assignment
          else
             T = struct2table(T,'AsArray',true); % for movevars, also simplifies field assignment
          end
 
-         if Aok % we have an attribute table, join it with S
+         if has_attrs_tbl % we have an attribute table, join it with S
             fields = S.fieldnames;
             for n = 1:numel(fields)
                T.(fields{n}) = A.(fields{n});
             end
          end
 
-         % might be able to remove this if updateBoundingBox is used
-         if contains('BoundingBox',T.Properties.VariableNames)
-            T = movevars(T,'BoundingBox','After','Geometry');
-         end
-
-         % If the lat/lon values are actually X, Y, rename the fields
-         if none(cellfun(@(x, y) isGeoGrid(y, x), lon, lat))
-            T = renamevars(T, {'Lat', 'Lon'}, {'Y', 'X'});
-         end
-
-         % send back the geostruct (overwrite m_shapefile S)
-         S = table2struct(T);
-
       catch ME4
 
       end
 
+   else
+
+      % Added this for shaperead so same checks below this can be applied
+      % whether m_map or shaperead was used.
+
+      % NOTE: If this causes trouble, Could move the
+
+      if ~isscalar(S)
+         T = struct2table(S);
+      else
+         T = struct2table(S, 'AsArray', true);
+      end
+
+      if all(ismember({'X', 'Y'}, T.Properties.VariableNames))
+         lat = T.Y;
+         lon = T.X;
+      else
+         % Easiest to use geostructCoordinates, but requires S not T. And if
+         % this fails, it will cause problems in the final checks, which may not
+         % even be necessary for shaperead ... but keep it for now.
+         [lat, lon] = geostructCoordinates(S, "geographic", "ascell");
+      end
    end
+
+   % BELOW HERE WAS ONLY IN THE M_MAP SECTION, SO MOVE IT BACK IF IT CREATES
+   % MORE PROBLEMS THAN IT SOLVES FOR SHAPEREAD.
+
+   % might be able to remove this if updateBoundingBox is used
+   if contains('BoundingBox', T.Properties.VariableNames)
+      T = movevars(T, 'BoundingBox', 'After', 'Geometry');
+   end
+
+   % If the lat/lon values are actually X, Y, rename the fields
+   if none(cellfun(@(x, y) isGeoGrid(y, x), lon, lat))
+
+      % Note: This was specifically for m_map b/c I think it always uses
+      % lat/lon?
+      T = renamevars(T, {'Lat', 'Lon'}, {'Y', 'X'});
+
+   elseif all(cellfun(@(x, y) isGeoGrid(y, x), lon, lat))
+
+      % Added this for a case where shaperead (w/o UseGeoCoords) returned X,Y
+      % but this requires checking if the fields are actually X,Y so I commented
+      % this out and INSTEAD I NEED TO ADD AN OPTION TO UPDATECOORDINATES TO DO
+      % THE RENAMING AND/OR REMEMBER TO USE USEGEOCOORDS
+
+      % T = renamevars(T, {'X', 'Y'}, {'Lon', 'Lat'});
+
+   else
+      % cannot determine if geographic or projected, revisit this later
+
+   end
+
+   % send back the geostruct (overwrite m_shapefile S)
+   S = table2struct(T);
+
 end
 
-function S = m_map_shaperead(fname,namedargs)
+function S = m_map_shaperead(fname, namedargs)
 
    if ~isactive('m_map')
       activate m_map
    end
 
-   if ismember({'BoundingBox'},namedargs(1:2:end)) % elements 1:2:end are parameters
-      B = namedargs{find(ismember('BoundingBox',namedargs(1:2:end)))+1};
-      S = m_shaperead(strrep(fname,'.shp',''),[B(1,:),B(2,:)]);
+   % Elements 1:2:end of namedargs are the parameter names
+   if ismember({'BoundingBox'}, namedargs(1:2:end))
+      B = namedargs{find(ismember('BoundingBox', namedargs(1:2:end)))+1};
+      S = m_shaperead(strrep(fname,'.shp',''), [B(1,:), B(2,:)]);
    else
-      S = m_shaperead(strrep(fname,'.shp',''));
+      S = m_shaperead(strrep(fname, '.shp', ''));
    end
 end
 
-% copied this here for matlab answers and changed mip to ip so keeping for now
-function S = geostructinit(geometry,numfeatures,varargin);
-   %geostructinit initializes a geostructure S of size 1xnumfeatures
-   %
-   %  Syntax
-   %
-   %  S = geostructinit(geometry,numfeatures);
-   %  Creates a geostruct S of size(1,numfeatures) and specified geometry
-   %  'line','point', or 'polygon'.
-   %
-   %  S = geostructinit(geometry,numfeatures,'fieldnames',fieldnames);
-   %  Creates a geostruct S of size(1,numfeatures) and specified geometry
-   %  'line','point', or 'polygon' and fields specified by 'fieldnames'
-   %  name-value input.
-   %
-   %  Author: Matt Cooper, Sep-23-2022, https://github.com/mgcooper
-   %
-   % See also:
+%% Input Parser
+function [fname, reader, UseGeoCoords, Selector, Attributes, ...
+      BoundingBox, namedargs] = parseinputs(fname, funcname, varargin)
 
-   % parse inputs
-   p = inputParser;
-   p.FunctionName = mfilename;
-   p.addRequired('geometry', @ischar);
-   p.addRequired('numfeatures', @isnumeric);
-   p.addParameter('fieldnames', '', @(x)ischar(x)|iscell(x) );
-   p.parse(geometry,numfeatures,varargin{:});
-   geometry = p.Results.geometry;
-   numfeatures = p.Results.numfeatures;
-   fieldnames = p.Results.fieldnames;
+   % in shaperead.m, a custom parseInputs function is used (not inputParser),
+   % which first initializes these as follows, therefore these should work as
+   % defualt values for this and any other function that calls shaperead.
+   %
+   % recordNumbers = [];
+   % boundingBox = [];
+   % selector = [];
+   % attributes = [];
+   % useGeoCoords = false;
 
-   % make sure the geometry is capitalized
-   geometry(1) = upper(geometry(1));
+   parser = inputParser();
+   parser.FunctionName = funcname;
+   parser.KeepUnmatched = true;
+   parser.addRequired('fname', @ischarlike);
+   parser.addOptional('reader', 'shaperead', @validreader);
+   parser.addParameter('UseGeoCoords', false, @islogical);
+   parser.addParameter('Selector', [], @iscell);
+   parser.addParameter('Attributes', [], @iscell);
+   parser.addParameter('BoundingBox', [], @isnumeric);
+   parser.parse(fname, varargin{:});
 
-   % init a geo struct
-   [S(1:numfeatures).Geometry] = deal(geometry);
-   [S(1:numfeatures).Lon] = deal(nan);
-   [S(1:numfeatures).Lat] = deal(nan);
+   reader = parser.Results.reader;
+   UseGeoCoords = parser.Results.UseGeoCoords;
+   Selector = parser.Results.Selector;
+   Attributes = parser.Results.Attributes;
+   BoundingBox = parser.Results.BoundingBox;
 
-   for n = 1:numel(fieldnames)
-      [S(1:numfeatures).(fieldnames{n})] = deal(nan);
-   end
+   % Jul 2024: This should work with refactored parser2varargin. Note the
+   % purpose here is to get the optional arguments which are NOT using defaults,
+   % because I don't know what the valid default values are for shaperead, so
+   % this way I can pass namedargs{:} to shaperead only using the arguments
+   % which are explicitly set by the user.
+   requiredargs = {'fname'};
+   namedargs = parser2varargin(parser, requiredargs, 'notusingdefaults');
+   % namedargs = namedargs2cell(parser.Results)
 end
 
-%% main parser
-function [fname, reader, UseGeoCoords, Selector, Attributes, BoundingBox, namedargs] = ...
-      parseinputs(fname, funcname, varargin)
-
-   p = inputParser;
-   p.FunctionName = funcname;
-   p.KeepUnmatched = true;
-
-   validreader = @(x)any(validatestring(x,{'shaperead','m_map'}));
-   addRequired(p, 'fname', @ischarlike);
-   addOptional(p, 'reader', 'shaperead', validreader);
-   addParameter(p, 'UseGeoCoords', true, @islogical);
-   addParameter(p, 'Selector', {}, @iscell);
-   addParameter(p, 'Attributes', {}, @iscell);
-   addParameter(p, 'BoundingBox', [], @isnumeric);
-   p.parse(fname,varargin{:});
-
-   reader = p.Results.reader;
-   UseGeoCoords = p.Results.UseGeoCoords;
-   Selector = p.Results.Selector;
-   Attributes = p.Results.Attributes;
-   BoundingBox = p.Results.BoundingBox;
-
-   namedargs = parser2varargin(p, {'fname'}, 'notusingdefaults');
-
-   % with this, only files returned by getgisfilelist will work, and since fname is
-   % required, I don't think we need to be strict like we would with addOptional
-   % validstrings = getgisfilelist;
-   % validfilename = @(x)any(validatestring(x,validstrings));
-
-   % this is how matlab validates filenames:
-   % validateattributes(filename,{'char'},{'vector'},mfilename,'FILENAME',1);
-
-   % UPDATE: I think namedargs2cell is what I need
-   % I thought I could pass arbitrary name-value pairs and use this to get them,
-   % but only the values are stored, not the names, so e.g. in this call:
-   % loadgis(fname,'UseGeoCoords',true)
-   % opts is a cell with one value 'true', not 'UseGeoCoords',true
-   % opts  = struct2cell(p.Unmatched);
-   % So for now, I use varargin
+function tf = validreader(x)
+   tf = any(validatestring(x, {'shaperead', 'm_map'}));
 end
-
-%%
-% I want a method to figure out which name-values are using defaults and then
-% NOT pass those to the nested function.
-
-
-% in shaperead.m, a custom parseInputs function is used (not inputParser), which
-% first initializes these as follows, therefore I should be able to use these as
-% defualt values for this and any other function that calls shaperead.
-% recordNumbers = [];
-% boundingBox = [];
-% selector = [];
-% attributes = [];
-% useGeoCoords = false;
-
-
-% % this was the previous method which assumed all files were in the top-level
-% USERGISPATH. To find full paths to subfolders, I added the addpath statement
-% above and use which.
-
-% % this is the root path of all gis files
-% datapath = getenv('USERGISPATH');
-%
-% [~,~,ext] = fileparts(fname);
-%
-% switch ext
-%    case '.shp'
-%       try
-%          Data = shaperead(fullfile(datapath,fname),varargin{:});
-%       catch ME
-%       end
-%    case '.tif'
-%       Data = readgeoraster(fullfile(datapath,fname),varargin{:});
-% end
