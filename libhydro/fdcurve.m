c663c20f8bd15f21d119ceab8be08295b7dc5351
mgcooper <mguycooper@gmail.com>
2024-07-24 12:30:07 -0700

    chore: Update style and small fixes.

--- libhydro/fdcurve.m
+++ libhydro/fdcurve.m
@@ -1,109 +1,117 @@
 function fdc = fdcurve(flow,varargin)
-%FDCURVE Flow duration curve
-%
-%     fdc = fdcurve(flow,varargin)
-%
-% See also
+   %FDCURVE Flow duration curve
+   %
+   %    fdc = fdcurve(flow)
+   %    fdc = fdcurve(flow, axscale=axscale)
+   %    fdc = fdcurve(flow, units=units)
+   %    fdc = fdcurve(flow, refpoints=refpoints)
+   %    fdc = fdcurve(flow, plotcurve=plotcurve)
+   %
+   % See also
 
-%-------------------------------------------------------------------------------
-p = magicParser;
-p.FunctionName=mfilename;
-p.addRequired('flow',@(x)isnumeric(x));
-p.addParameter('axscale','semilogy',@(x)ischar(x));
-p.addParameter('units','',@(x)ischar(x));
-p.addParameter('refpoints',nan,@(x)isnumeric(x));
-p.addParameter('plotcurve',true,@(x)islogical(x));
-p.parseMagically('caller');
-%-------------------------------------------------------------------------------
+   % Parse inputs
+   parser = inputParser;
+   parser.FunctionName = mfilename;
+   parser.addRequired('flow', @isnumeric);
+   parser.addParameter('axscale', 'semilogy', @isscalartext);
+   parser.addParameter('units', '', @isscalartext);
+   parser.addParameter('refpoints', nan, @isnumeric);
+   parser.addParameter('plotcurve', true, @islogicalscalar);
+   parser.parse(flow, varargin{:});
+   kwargs = parser.Results;
+   [axscale, units, refpoints, plotcurve] = deal(kwargs.axscale, ...
+      kwargs.units, kwargs.refpoints, kwargs.plotcurve);
 
-N     = length(flow);
-M     = 1:N;
-x     = sort(flow,'descend');
-f     = 1-M./(N+1);
+   % Main algorithm
+   N = length(flow);
+   M = 1:N;
+   x = sort(flow,'descend');
+   f = 1-M./(N+1);
 
-% if requested, compute reference point values
-xref = nan(numel(refpoints,1));
-fref = nan(numel(refpoints,1));
-if ~isnan(refpoints)
-   for n = 1:numel(refpoints)
-      iref     = find(x>=refpoints(n),1,'last');
-      xref(n)  = x(iref);
-      fref(n)  = f(iref);
-   end
-end
-
-% plot the curve if requested
-if plotcurve == true
-   figure;
-   switch axscale
-      case 'loglog'
-         h.fdc = loglog(100.*f,x); ax = gca;
-      case 'semilogy'
-         h.fdc = semilogy(100.*f,x); ax = gca;
-      case 'semilogx'
-         h.fdc = semilogx(100.*f,x); ax = gca;
-      case 'linear'
-         h.fdc = plot(100.*f,x); ax = gca;
-   end
-
-   % if requested, add a refpoint line
+   % If requested, compute reference point values
+   xref = nan(numel(refpoints,1));
+   fref = nan(numel(refpoints,1));
    if ~isnan(refpoints)
-      hold on;
       for n = 1:numel(refpoints)
-         xplot    = [min(xlim) 100*fref(n) 100*fref(n) 100*fref(n)];
-         yplot    = [xref(n) xref(n) min(ylim) xref(n)];
-         h.ref(n) = plot(xplot,yplot,'Color',[0.85 0.325 0.098],'LineWidth',1);
+         iref = find(x >= refpoints(n),1,'last');
+         xref(n) = x(iref);
+         fref(n) = f(iref);
       end
    end
 
-   ylabel(['$x$ [' units ']']);
-   xlabel 'flow exceedence probability, $P(Q\ge x)$'
+   % Plot the curve if requested
+   if plotcurve
+      figure
+      switch axscale
+         case 'loglog'
+            h.fdc = loglog(100.*f,x); ax = gca;
+         case 'semilogy'
+            h.fdc = semilogy(100.*f,x); ax = gca;
+         case 'semilogx'
+            h.fdc = semilogx(100.*f,x); ax = gca;
+         case 'linear'
+            h.fdc = plot(100.*f,x); ax = gca;
+      end
 
-   ax.YAxis.TickLabels = compose('%g',ax.YAxis.TickValues);
-   ax.XAxis.TickLabels = compose('$%g\\%%$',ax.XAxis.TickValues);
+      % if requested, add a refpoint line
+      if ~isnan(refpoints)
+         hold on
+         for n = 1:numel(refpoints)
+            xplot = [min(xlim) 100*fref(n) 100*fref(n) 100*fref(n)];
+            yplot = [xref(n) xref(n) min(ylim) xref(n)];
+            h.ref(n) = plot(xplot,yplot,'Color',[0.85 0.325 0.098],'LineWidth',1);
+         end
+      end
 
-   % since i manually set the ticklabels, i think this is necessary
-   % otherwise if the figure is resized, matlab will make new ticks
-   set(gca,'XTickMode','manual','YTickMode','manual');
+      ylabel(['$x$ [' units ']']);
+      xlabel 'flow exceedence probability, $P(Q\ge x)$'
 
-   figformat('suppliedline',h.fdc,'linelinewidth',3);
+      ax.YAxis.TickLabels = compose('%g',ax.YAxis.TickValues);
+      ax.XAxis.TickLabels = compose('$%g\\%%$',ax.XAxis.TickValues);
 
-   fdc.h    = h;
+      % since i manually set the ticklabels, i think this is necessary
+      % otherwise if the figure is resized, matlab will make new ticks
+      set(gca,'XTickMode','manual','YTickMode','manual');
+
+      figformat('suppliedline',h.fdc,'linelinewidth',3);
+
+      fdc.h = h;
+   end
+
+   % package output
+   fdc.f = f;
+   fdc.x = x;
+   fdc.xref = xref;
+   fdc.fref = fref;
+
+   % % this would also work, adapted from num2ltext
+   % ax.XAxis.TickLabels = cellstr(num2str(ax.XAxis.TickValues','$%g\\%%$'))
+   %
+   % % thought I could let matlab compose them and then get the ticklabels:
+   % ax.XAxis.TickLabelInterpreter = 'latex';
+   % xtickformat 'percentage'
+   % % but they disappear when latex is set
 end
 
-% package output
-fdc.f    = f;
-fdc.x    = x;
-fdc.xref = xref;
-fdc.fref = fref;
-
-%    % this would also work, adapted from num2ltext
-%    ax.XAxis.TickLabels = cellstr(num2str(ax.XAxis.TickValues','$%g\\%%$'))
-
-%    % thought I could let matlab compose them and then get the ticklabels:
-%    ax.XAxis.TickLabelInterpreter = 'latex';
-%    xtickformat 'percentage'
-%    % but they disappear when latex is set
-
-
-function [F,x] = ecdfpot(x,xmin,alpha,sigma)
-
-% not implemented
-
-% http://sfb649.wiwi.hu-berlin.de/fedc_homepage/xplore/tutorials/sfehtmlnode91.html
-
-% peaks over threshold exceedance probability:
-% F(x) = N(u)/n(1+gamma(x-u)/beta)^(-1/gamm),
-% N(u) is the number of obs>u
-% n is totla number, i think
-% x would be tau
-% u would be taumin
-% gamma/beta would be b-1/tau0 i think
-% so 1/gamm would be 1/b-1
-
-
-N     = numel(x(x>xmin));
-n     = numel(x);
-gamma = b-1; % might be 1-b;
-beta  = tau0;
-F     = N/n.*(1+gamma.*(x-x0)/beta)^(-1/gamm);
+% function [F,x] = ecdfpot(x,xmin,alpha,sigma)
+%
+%    % not implemented
+%
+%    % http://sfb649.wiwi.hu-berlin.de/fedc_homepage/xplore/tutorials/sfehtmlnode91.html
+%
+%    % peaks over threshold exceedance probability:
+%    % F(x) = N(u)/n(1+gamma(x-u)/beta)^(-1/gamm),
+%    % N(u) is the number of obs>u
+%    % n is totla number, i think
+%    % x would be tau
+%    % u would be taumin
+%    % gamma/beta would be b-1/tau0 i think
+%    % so 1/gamm would be 1/b-1
+%
+%
+%    N = numel(x(x>xmin));
+%    n = numel(x);
+%    gamma = b-1; % might be 1-b;
+%    beta = tau0;
+%    F = N/n.*(1+gamma.*(x-x0)/beta)^(-1/gamm);
+% end
