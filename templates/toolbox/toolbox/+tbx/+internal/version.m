b0e971159db33be7e35b2ce75159869d40f8f8b2
mgcooper <mguycooper@gmail.com>
2024-07-24 20:44:39 -0700

    chore: Migrate some updates from groupstats/+internal versions.

--- templates/toolbox/toolbox/+tbx/+internal/version.m
+++ templates/toolbox/toolbox/+tbx/+internal/version.m
@@ -1,13 +1,13 @@
-function version = version(varargin)
-   %GETVERSION read version.txt in the toolbox root directory
+function v = version()
+   %GETVERSION Read version.txt in the toolbox root directory.
    %
-   %  version = tbx.internal.version()
+   %  v = tbx.internal.version()
    %
    % See also:
 
    try
-      version = fileread(tbx.internal.projectpath(), 'version.txt');
+      v = fileread(projectpath(), 'version.txt');
    catch
-      version = 'v0.1.0';
+      v = 'v0.1.0';
    end
 end
