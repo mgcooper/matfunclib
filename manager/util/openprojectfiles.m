893ed6562293299fea72b13c0f2854734a42d384
mgcooper <mguycooper@gmail.com>
2024-07-26 22:04:17 -0700

    refactor(manager)!: Replace USERPROJECTPATH with MATLAB_PROJECTS_PATH.

--- manager/util/openprojectfiles.m
+++ manager/util/openprojectfiles.m
@@ -1,23 +1,27 @@
 function openprojectfiles(projectname)
-%OPENPROJECTFILES open project files 
+   %OPENPROJECTFILES open project files
+   %
+   % Note:
 
-if nargin < 1
-   projectname = getactiveproject();
+   if nargin < 1
+      projectname = getactiveproject();
+   end
+
+   activefiles = getprojectfiles(projectname);
+
+   % open the project files that are not already open
+   if ~isempty(activefiles)
+
+      openfiles = getopenfiles();
+      activefiles = activefiles(~ismember(activefiles, openfiles));
+      reopenfiles(activefiles);
+
+      % close all currently open files before reopening the project files
+      % openfiles.close;
+   end
+
+   % % commented out see setprojectfiles
+   % if nargin == 1
+   %    projlist = readprjdirectory;
+   % end
 end
-
-activefiles = getprojectfiles(projectname);
-
-% open the project files that are not already open
-if ~isempty(activefiles)
-   openfiles = getopenfiles();
-   activefiles = activefiles(~ismember(activefiles,openfiles));
-   reopenfiles(activefiles);
-
-   % close all currently open files before reopening the project files
-   % openfiles.close;
-end
-
-% % commented out see setprojectfiles
-% if nargin == 1
-%    projlist = readprjdirectory;
-% end
