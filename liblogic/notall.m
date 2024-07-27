c9b222013df774092a35508d17b5d22a0cf27661
mgcooper <mguycooper@gmail.com>
2024-07-27 00:35:53 -0700

    feat(notall): Add varargin to pass any arg to "all".

--- liblogic/notall.m
+++ liblogic/notall.m
@@ -1,4 +1,11 @@
-function TF = notall(X)
+function TF = notall(X, varargin)
    %NOTALL return TRUE if ~all(X) is TRUE
-   TF = ~all(X);
+   %
+   %  TF = notall(X)
+   %  TF = notall(X, 'all')
+   %  TF = notall(X, dim)
+   %  TF = notall(X, vecdim)
+   %
+   % See also: none notempty notequal
+   TF = ~all(X, varargin{:});
 end
