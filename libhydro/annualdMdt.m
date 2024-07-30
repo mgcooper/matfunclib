c4e2906438906482f185bd5fefd0d185a3ba331a
mgcooper <mguycooper@gmail.com>
2024-07-30 11:49:57 -0700

    fix(annualdMdt): Do not apply averaging if bimonthly=false.
    Also update / clarify docs and add "ascolumn" option.

--- libhydro/annualdMdt.m
+++ libhydro/annualdMdt.m
@@ -1,34 +1,43 @@
 function [dMdt, Mbar, M, Time] = annualdMdt(M, Time, kwargs)
    %ANNUALDMDT Compute annual differences from monthly values of stored mass.
    %
-   %    [DMDT, MT] = ANNUALDMDT(M, TIME)
+   %    [dMdt, Mbar, M, Time] = annualdMdt(M, Time)
+   %    [dMdt, Mbar, M, Time] = annualdMdt(M, Time, bimonthly=false)
+   %    [dMdt, Mbar, M, Time] = annualdMdt(M, Time, extraside="left")
+   %    [dMdt, Mbar, M, Time] = annualdMdt(M, Time, extraside="right")
+   %    [dMdt, Mbar, M, Time] = annualdMdt(M, Time, astimetables=false)
+   %    [dMdt, Mbar, M, Time] = annualdMdt(M, Time, ascolumn=true)
    %
    % Description:
    %
-   %    [DMDT, MT] = ANNUALDMDT(M, TIME) Computes annual differences from
-   %    monthly values of mass, e.g., gravimetric water storage, climate model
-   %    snow mass, etc. Input data are assumed to be a timeseries [months x 1]
+   %    [DMDT, MT] = ANNUALDMDT(M, TIME) Computes annual differences DMDT from
+   %    monthly values of mass M, e.g., gravimetric water storage, climate model
+   %    snow mass, etc. Input data M are assumed to be a timeseries [months x 1]
    %    of monthly averages nominally posted mid-month; bimonthly averaging
    %    creates monthly values posted at the month start. Annual differences are
    %    posted to the START month/year of each annual period, i.e., differences
-   %    are in a 12-month "forward" sense, where the difference for year_i is:
+   %    are in a 12-month "forward" sense, where the 12-month forward difference
+   %    for month j of year i is:
    %
-   %     dMdt(MMM)_year_i = M(MMM)_year_(i+1) - M(MMM)_year_(i).
+   %     dMdt(i, j) = M(i+1, j) - M(i, j).
    %
-   %    Here, M is mass and MMM indexes months. Differences are posted monthly
-   %    such that the annual calendar-year difference for year i would be
-   %    obtained from the January values:
+   %    Forward differences are computed beginning each month such that the
+   %    annual calendar-year difference for year i would be obtained from the
+   %    January values:
    %
-   %     dMdt_calyear_i = M(Jan)_year_(i+1) - M(Jan)_year_(i).
+   %     dMdt_calyear_i = M(i+1, j(Jan, i+1)) - M(i, j(Jan, i)).
    %
-   %    Here, dMdt_calyear_i represents the change in mass during year i. See
-   %    notes below about water years.
+   %    Here, dMdt_calyear_i represents the change in mass during year i, and
+   %    j(Jan, i+1) and j(Jan, i) are indices for the month of January in year
+   %    i+1 and year i, respectively.
+   %
+   %    See notes below about water years.
    %
    % Inputs:
    %
    %    TIME - A monthly datetime calendar, ideally with one extra month on
    %    either side to compute the first and last bi-monthly average. For
-   %    instance, to obtain an accurate calendar year difference for year(i),
+   %    instance, to obtain an accurate calendar-year difference for year(i),
    %    TIME should begin Dec of year(i-1), so the bi-monthly average value for
    %    Jan of year(i) can be computed, by averaging the value for Dec of
    %    year(i-1) and the value for Jan of year(i). Similarly, to compute a
@@ -38,23 +47,34 @@ function [dMdt, Mbar, M, Time] = annualdMdt(M, Time, kwargs)
    %    one extra month on either side.
    %
    %    The function automatically accounts for the presence or absence of extra
-   %    values on either side. If TIME includes only one extravalue on one side,
-   %    the user should supply the "extraside" parameter to specify which side
-   %    contains the extravalue. The values returned by this function will not
-   %    include these extra values; values are posted beginning Jan of year(i)
-   %    and ending Dec of year(N) (or beginning on whichever month marks the
-   %    first month and ending on the month 12 months later).
+   %    months on either side. If TIME includes only one extra month on one
+   %    side, the user should supply the "extraside" parameter to specify which
+   %    side contains the extra value. The values returned by this function will
+   %    not include these extra values; values are posted beginning Jan of
+   %    year(i) and ending Dec of year(N) (or beginning on whichever month marks
+   %    the first month and ending on the month 12 months later). To omit
+   %    bimonthly averaging altogether, specify BIMONTHLY=FALSE.
    %
    %    M - A vector or matrix of a mass (or other conserved quantity),
    %    nominally registered to the middle of each month in Time.
    %
    %    BIMONTHLY - (optional, name-value) Logical flag to control whether
    %    bimonthly averaging of neighboring months is performed to compute
-   %    month-start values.
+   %    month-start values. The default value is TRUE.
    %
    %    EXTRASIDE - (optional, name-value) String indicating which side ("left"
    %    or "right") has an extra value for bi-monthly averaging. Use this option
-   %    to resolve the ambiguity when only one side has an extra value.
+   %    to resolve the ambiguity when only one side has an extra value. The
+   %    default value is BOTH.
+   %
+   %    ASTIMETABLES - (optional, name-value) Logical flag to control whether
+   %    outputs DMDT and MBAR are returned as timetables. The default value is
+   %    TRUE.
+   %
+   %    ASCOLUMN - (optional, name-value) Logical flag to control whether
+   %    outputs DMDT and MBAR are returned as column vectors, i.e., in the same
+   %    format as input M, TIME, or as 2d arrays with dimensions [year month].
+   %    The default value is FALSE.
    %
    % Outputs:
    %
@@ -63,6 +83,9 @@ function [dMdt, Mbar, M, Time] = annualdMdt(M, Time, kwargs)
    %    considering each month as the beginning of 12-month forward periods such
    %    that annual differences can be obtained for arbitrary definitions of
    %    "years" (e.g., "water years" beginning Oct 1).
+   %
+   %    MBAR - Bimonthly average M, returned as a timetable of size [years x
+   %    12]. Specify ASCOLUMN=TRUE to return as [months x 1].
    %
    %  ** NOTE ON WATER YEARS **
    %
@@ -94,25 +117,25 @@ function [dMdt, Mbar, M, Time] = annualdMdt(M, Time, kwargs)
    %    bi-monthly averages must "go into" the SECOND month, i.e., the index on
    %    the LHS of the following expressions equals the second index on the RHS:
    %
-   %     Sbar_jan(i+1) = ( S_dec(i)   + S_jan(i+1) ) / 2
-   %     Sbar_jan(i)   = ( S_dec(i-1) + S_jan(i) )   / 2
-   %     dS/dt(i)      =   S_jan(i+1) - S_jan(i)
+   %     dS/dt(i)      = Sbar_jan(i+1)  -  Sbar_jan(i)
+   %     Sbar_jan(i+1) = (  S_dec(i)    +  S_jan(i+1) ) / 2
+   %     Sbar_jan(i)   = (  S_dec(i-1)  +  S_jan(i)   ) / 2
    %
    %    If averages "go into" the first month, the bi-monthly averaging remains
    %    the same, but differences would need to be computed as:
    %
-   %     dS/dt(i)      =   S_dec(i) - S_dec(i-1)
+   %     dS/dt(i)      = Sbar_dec(i)    -  Sbar_dec(i-1)
    %
    %    Bi-monthly averaging requires an extra month of data on either side of
    %    the period of record, i.e., to compute the value for January of year i,
-   %    where i=1, the value from December of year i-1 is required.
+   %    the value from December of year i-1 is required.
    %
    %    An extra month is not strictly required to compute the bi-monthly
-   %    average for the final month, since it is averaged with its left
-   %    neighbor. For example, the final Dec bi-monthly average is the average
-   %    of the final Nov and Dec values. However, to compute the final annual
-   %    difference, it becomes necessary to have an extra month (Jan following
-   %    the final Dec) to compute the annual difference for the final year.
+   %    average for the final month, since it is averaged with its left neighbor
+   %    (the final Dec bi-monthly average is the average of the final Nov and
+   %    Dec values). However, to compute the final annual difference, it becomes
+   %    necessary to have an extra month (Jan following the final Dec) to
+   %    compute the annual difference for the final year.
    %
    %    To illustrate, if the period of record is 2000-2020, this function will
    %    return annual differences for annual periods beginning each month,
@@ -125,17 +148,16 @@ function [dMdt, Mbar, M, Time] = annualdMdt(M, Time, kwargs)
    %    And to compute the annual difference for year 2020:
    %     dS/dt(2020) = (Sdec(2020)+Sjan(2021))/2 - (Sdec(2019)+Sjan(2020))/2
    %
-   %    Thus the algorithm requires an extra month on either side of the first
-   %    and last month in the period of record (Sdec(1999) and Sjan(2021) in the
-   %    example above).
+   %    Thus the algorithm requires two extra months (Sdec(1999) and Sjan(2021)
+   %    in the above example), one each on either side of the first and last
+   %    month in the period of record.
    %
    %    If the number of months in TIME is an even divisor of 12, then bimonthly
-   %    averaging is NOT performed for the first and last monthly values, but is
+   %    averaging is NOT performed for the first and last monthly values, but IS
    %    performed for all months in between. To turn of bimonthly averaging
    %    altogether, set the optional name-value parameter BIMONTHLY=FALSE.
    %
    % See also:
-
 
    arguments
       M (:, 1)
@@ -145,146 +167,127 @@ function [dMdt, Mbar, M, Time] = annualdMdt(M, Time, kwargs)
          kwargs.extraside, ["left", "right", "both", "none", "unknown"])} ...
          = "unknown"
       kwargs.astimetables (1, 1) logical = true
+      kwargs.ascolumn (1, 1) logical = false
       kwargs.plotflag (1, 1) logical = false
    end
 
-   % Prepare the vector for bimonthly averaging.
-   if kwargs.bimonthly
-      [M, Time] = prepareBimonthly(M, Time, kwargs.extraside);
-   end
+   % Prepare the vector for bimonthly averaging (or not).
+   [M, Time] = prepareBimonthly(M, Time, kwargs.extraside, kwargs.bimonthly);
 
+   % Initialize outputs.
    N = length(M);
    Mbar = nan(N, 1);
    dMdt = nan(N, 1);
 
-   % Compute bi-monthly averages.
-   for i = 1:N-1
-      Mbar(i+1) = (M(i) + M(i+1)) / 2;
+   % Compute bi-monthly averages (or not).
+   if kwargs.bimonthly
+      for i = 1:N-1
+         Mbar(i+1) = (M(i) + M(i+1)) / 2;
+      end
+   else
+      Mbar = M;
    end
 
-   % Compute annual differences of bi-monthly averages on a monthly basis.
+   % Compute 12-month forward annual differences on a monthly basis.
    for i = 1:(N-12)-1
       ii = i+12;
       dMdt(i+1) = Mbar(ii+1)-Mbar(i+1);
    end
 
    % Remove the extra values on either side used for bi-monthly averaging.
-   [Mbar, dMdt, M, Time] = deal(Mbar(2:N-1), dMdt(2:N-1), M(2:N-1), Time(2:N-1));
+   [Mbar, dMdt, M, Time] = deal( ...
+      Mbar(2:N-1), dMdt(2:N-1), M(2:N-1), Time(2:N-1));
 
    % Plot the result if requested.
    if kwargs.plotflag
-      figure
-      hold on
-      plot(Time, Mbar, '-')
-      plot(Time, M, ':')
-      legend('Bimonthly Averages', 'Original Data')
+      createfigure(Time, M, Mbar)
    end
 
-   if kwargs.astimetables
-      % Build an annual calendar.
-      numyears = (N-2) / 12;
-      newtimes = transpose(Time(1):calyears(1):Time(1)+calyears(numyears-1));
-      months = cellstr(datestr(Time(1:12), 'mmm'));
+   % Prepare the outputs
+   [Mbar, dMdt] = prepareOutputs(Mbar, dMdt, M, Time, kwargs);
+end
 
-      % Reshape to a matrix [years x months].
+%%
+function [M, T] = prepareBimonthly(M, T, extraside, bimonthly)
+
+   nmonths = length(M);
+
+   if not(bimonthly)
+
+      assert(mod(nmonths, 12) == 0, ['input data must be posted monthly, ' ...
+         'for an even number of annual periods.']);
+   else
+
+      if mod(nmonths, 12) == 0
+
+         % Repeat the first and last month to ensure length(M) == nmonths+2.
+         % Bimonthly averaging of these repeats will return the original values.
+         M = [M(1); M; M(end)];
+         T = [T(1); T; T(end)];
+
+      elseif mod(nmonths - 1, 12) == 0
+
+         assert(strcmp(extraside, {'left', 'right'}))
+
+         % Repeat the missing side so averaging yields the original value.
+         switch extraside
+            case "left"
+               M = [M; M(end)];
+               T = [T; T(end)];
+            case "right"
+               M = [M(1); M];
+               T = [T(1); T];
+         end
+
+      elseif mod(nmonths-2, 12) == 0
+         % allow extraside to be unknown, assume it is "both".
+
+      elseif mod(nmonths,12) ~= 0
+         error( ...
+            ['input data must be posted monthly, for an even number of ' ...
+            'annual periods, with an optional extra month on either side.'])
+      end
+   end
+end
+%%
+function createfigure(Time, M, Mbar)
+   figure
+   hold on
+   plot(Time, Mbar, '-')
+   plot(Time, M, ':')
+   legend('Bimonthly Averages', 'Original Data')
+end
+%%
+
+function [Mbar, dMdt] = prepareOutputs(Mbar, dMdt, M, Time, kwargs)
+
+   N = length(M);
+
+   % Keep as a column [year*month x 1] or return a matrix [year x month]
+   if kwargs.ascolumn
+
+      if kwargs.astimetables
+         % Return timetables with a single monthly column:
+         Mbar = timetable(Mbar, 'RowTimes', Time);
+         dMdt = timetable(dMdt, 'RowTimes', Time);
+      end
+
+   else
+      % Reshape to a matrix [year x month].
+      numyears = (N-2) / 12;
       Mbar = transpose(reshape(Mbar, 12, numyears));
       dMdt = transpose(reshape(dMdt, 12, numyears));
 
-      % Create timetables
-      Mbar = array2timetable(Mbar, 'RowTimes', newtimes, 'VariableNames', months);
-      dMdt = array2timetable(dMdt, 'RowTimes', newtimes, 'VariableNames', months);
-   end
+      % Build an annual calendar.
+      times = transpose(Time(1):calyears(1):Time(1)+calyears(numyears-1));
+      month = cellstr(datestr(Time(1:12), 'mmm'));
 
-   % To return a timetable as a single monthly column:
-   % dMdt = timetable(dMdt, 'RowTimes', Time);
-end
+      % Convert to timetables unless otherwise requested.
+      if kwargs.astimetables
 
-function [M, T] = prepareBimonthly(M, T, extraside)
-
-   nmonths = length(M);
-   if mod(nmonths, 12) == 0
-
-      % Repeat the first and last month to ensure length(M) == nmonths+2.
-      % Bi-monthly averaging of these repeats will return the original values.
-      M = [M(1); M; M(end)];
-      T = [T(1); T; T(end)];
-
-   elseif mod(nmonths - 1, 12) == 0
-
-      assert(strcmp(extraside, {'left', 'right'}))
-
-      % Repeat the missing side so averaging yields the same value.
-      switch extraside
-         case "left"
-            M = [M; M(end)];
-            T = [T; T(end)];
-         case "right"
-            M = [M(1); M];
-            T = [T(1); T];
+         % Create timetables
+         Mbar = array2timetable(Mbar, 'RowTimes', times, 'VariableNames', month);
+         dMdt = array2timetable(dMdt, 'RowTimes', times, 'VariableNames', month);
       end
-
-   elseif mod(nmonths-2, 12) == 0
-      % allow extraside to be unknown, assume it is "both".
-
-   elseif mod(nmonths,12) ~= 0
-      error(['input data must be posted monthly, for an even number of ' ...
-         'annual periods, optionally with +1 extra month on one or both sides']);
    end
-end
-
-function debugcalc(Mbar, dMdt)
-
-   % Compute bi-monthly averages. Note: The values are later registered to Time.
-   for i = 1:N-1
-      Mbar(i+1) = (M(i) + M(i+1)) / 2;
-
-      % dMdt(i+1) = (M(ii)+M(ii+1))/2 - (M(i)+M(i+1))/2;
-      % where ii = i+12
-   end
-
-   % Compute annual differences on a monthly basis.
-   for i = 1:(N-12)-1
-
-      ii = i+12;
-      dMdt(i+1) = Mbar(ii+1)-Mbar(i+1);
-
-      % Here ii and ii+1 represent the 12 month forward bimonthly average, and
-      % i and i+1 represent the start bimonthly average.
-      %  dMdt(i+1) = (M(ii)+M(ii+1))/2 - (M(i)+M(i+1))/2;
-      %
-      % For example, if i=1 is Dec 2000:
-      %  dMdt(Jan2001) = (M(Dec2001)+M(Jan2002))/2 - (M(Dec2000)+M(Jan2001))/2;
-      %
-      % Or in terms of Mbar:
-      %  dMdt(i+1) = Mbar(ii+1) - Mbar(i+1);
-      %  dMdt(Jan2001) = Mbar(Jan2002) - Mbar(Jan2001);
-      %
-      % At the end of the timeseries, say Jan 2021
-      %  dMdt(Jan2021) = (M(Dec2021)+M(Jan2022))/2 - (M(Dec2020)+M(Jan2021))/2;
-      %  dMdt(Jan2021) = Mbar(Jan2022) - Mbar(Jan2021);
-      %
-      % For indexing, for N=nmonths+2, where Jan2022 is i=N, and the final
-      % annual difference is one year prior, Jan2021:
-      %  dMdt(N-12) = (M(N-1)+M(N))/2 - (M(N-12-1)+M(N-12))/2;
-      % Or
-      %  dMdt(N-12) = Mbar(N) - Mbar(N-12)
-      %
-      % Say i=(N-12)-1, then ii=N-1, and we want:
-      %  dMdt(i+1) = Mbar(ii+1) - Mbar(i+1);
-      % Plug in for i and ii in terms of N:
-      %  dMdt(N-12) = Mbar(N) - Mbar(N-12);
-      % Which is the correct result.
-   end
-
-   % % This is the orignal method that uses the Jan + Dec only
-   % for n = 2:numel(years)-1
-   %    iyear = years(n);
-   %    ideci = find(Time == datetime(iyear,12,1));
-   %    ijanip1 = find(Time == datetime(iyear+1,1,1));
-   %    idecim1 = find(Time == datetime(iyear-1,12,1));
-   %    ijani = find(Time == datetime(iyear,1,1));
-   %
-   %    dMdt(n) = (S(ideci)+S(ijanip1))/2 - (S(idecim1)+S(ijani))/2;
-   % end
-   % dMdt = timetable(dMdt, 'RowTimes', datetime(years,1,1));
 end
