b270294964051981b62cab76796c037469ccbcb5
mgcooper <mguycooper@gmail.com>
2024-07-31 06:40:34 -0700

    feat(annualMinMax): Add "allowmissing" months and return time.
    Previously the data were assumed to be complete 12-month periods. Tried to add method to pad with nan, may not work. Also return the time vector if modified.

--- libhydro/annualMinMax.m
+++ libhydro/annualMinMax.m
@@ -1,4 +1,4 @@
-function [GL, GH, imin, imax] = annualMinMax(G, time, varargin)
+function [GL, GH, imin, imax, time] = annualMinMax(G, time, varargin)
    %ANNUALMINMAX Compute annual minimum and maximum values of monthly data.
    %
    %    [GL, GH, imin, imax] = annualMinMax(G, time)
@@ -10,7 +10,7 @@ function [GL, GH, imin, imax] = annualMinMax(G, time, varargin)
    %
    % Description
    %
-   %    [GL, GH, imin, imax] = annualMinMax(G, time)
+   %    [GL, GH, imin, imax, time] = annualMinMax(G, time)
    %
    % See also: snowMassCorrection graceGapFill
 
@@ -24,11 +24,27 @@ function [GL, GH, imin, imax] = annualMinMax(G, time, varargin)
 
    % Adjust to cal years vs water year
    if opts.aswateryears
+      t1 = datetime(year(time(1)), 10, 1);
+      t2 = datetime(year(time(end)), 9, 30);
       i1 = find(month(time) == 10, 1, 'first');
       i2 = find(month(time) == 9, 1, 'last');
    else
+      t1 = datetime(year(time(1)), 1, 1);
+      t2 = datetime(year(time(end)), 12, 31);
       i1 = find(month(time) == 1, 1, 'first');
       i2 = find(month(time) == 12, 1, 'last');
+   end
+
+   nmonths = numel(time);
+
+   if opts.allowmissing
+      % Pad either end with nan to create an even number of annual periods
+      if mod(nmonths, 12) ~= 0
+         [G, time] = makeRegularTime(G, time, t1, t2);
+      end
+   else
+      assert(mod(nmonths, 12) == 0, ['Input data must be posted monthly, ' ...
+         'for an even number of annual periods. Set allowmissing=true to override.']);
    end
 
    % Reshape G to [month x year]
@@ -64,6 +80,8 @@ function [GL, GH, imin, imax] = annualMinMax(G, time, varargin)
    if opts.plotfig
       makeMinMaxPlot(G, imin, imax, opts)
    end
+
+   time = time(i1:i2);
 end
 
 %%
@@ -108,6 +126,7 @@ function [G, Time, opts] = parseinputs(G, Time, mfilename, varargin)
 
    parser = inputParser;
    parser.FunctionName = mfilename;
+   parser.KeepUnmatched = true;
    parser.addRequired('G', @isnumericvector);
    parser.addRequired('Time', @isdatelike);
    parser.addParameter('aswateryears', false, @islogicalscalar);
@@ -116,7 +135,51 @@ function [G, Time, opts] = parseinputs(G, Time, mfilename, varargin)
    parser.addParameter('plotcheck', false, @islogicalscalar);
    parser.addParameter('validmonthsmin', 1:12, @isnumericvector);
    parser.addParameter('validmonthsmax', 1:12, @isnumericvector);
+   parser.addParameter('allowmissing', false, @islogicalscalar)
    parser.parse(G, Time, varargin{:});
 
    opts = parser.Results;
+end
+
+%%
+function [G, time] = makeRegularTime(G, time, t1, t2)
+
+   % This works if each element of time is the first of each month.
+   % Otherwise, this will fail. Revisit later with a robust method to pad
+   % missing months in the first and last year. For now, pass in complete
+   % annual periods.
+
+   newtime = t1:calmonths(1):t2;
+   ttbl = timetable(G, 'RowTimes', time);
+   ttbl = retime(ttbl, newtime, "fillwithmissing");
+   [G, time] = deal(ttbl.G, ttbl.Time);
+
+   return
+
+   %#ok<*UNRCH>
+
+   % Below here testing
+
+   % To determining padding, make time a regular calendar
+   nomissing_time = (t1:calmonths(1):t2)';
+   regular_time = dateshift(time, 'start', 'month');
+   missing_times = ~ismember(nomissing_time, regular_time);
+   found_time = ismember(nomissing_time, regular_time);
+
+   % This doesn't work as expected, it doesn't simply fill in the found
+   % slots with the original, irregular time. So I moved on.
+   newtime = nomissing_time;
+   newtime(found_time) = time;
+
+   % This may not be needed with the method above. This handled the case
+   % where the first month was say Jan 16, but it failed b/c subtracting
+   % one calmonth doesn't work if day(time(1)) ~= 1, but caldays may work,
+   % but I developed the method above this instead.
+
+   if month(t1) ~= month(time(1)) && day(time(1)) ~= 1
+      padstart = t1:calmonths(1):time(1)-calmonths(1);
+   end
+   if month(t2) ~= month(time(end)) && day(time(end)) ~= 1
+      padend = t2:calmonths(1):time(1)-calmonths(1);
+   end
 end
