e27424aa1dd764b86b46a6af8b3c1ad3910f5432
mgcooper <mguycooper@gmail.com>
2024-08-07 16:40:50 -0700

    feat(functemplate_libtable): Add wastabular flag.

--- libtable/functemplate_libtable.m
+++ libtable/functemplate_libtable.m
@@ -5,27 +5,45 @@ function tbls = functemplate_libtable(tbls)
    %
    % See also:
 
-   [tbls, wasstruct, names] = parseinputs(tbls);
+   % Prepare inputs.
+   [tbls, wastabular, wasstruct, names] = parseinputs(tbls);
 
+   % Process each table.
    tbls = cellmap(@(tbl) processOneTable(tbl), tbls);
 
-   if wasstruct
-      tbls = cell2struct(tbls, names, 1);
-   else
-      tbls = tbls{:};
-   end
+   % Prepare outputs.
+   tbls = parseoutputs(tbls, wastabular, wasstruct, names);
 end
 
+%%
 function tbl = processOneTable(tbl)
 
 end
 
 %%
-function [tbls, wasstruct, names] = parseinputs(tbls)
+function [tbls, wastabular, wasstruct, names] = parseinputs(tbls)
+
+   names = string.empty;
    wasstruct = isstruct(tbls);
-   if wasstruct
-      [names, tbls] = deal(string(fieldnames(tbls)), struct2cell(tbls));
-   elseif istabular(tbls)
+   wastabular = istabular(tbls);
+
+   if iscell(tbls)
+      assert(all(cellfun(@istabular, Merra)))
+
+   elseif wasstruct
+      [tbls, names] = deal(struct2cell(tbls), string(fieldnames(tbls)));
+
+   elseif wastabular
       tbls = {tbls};
+   end
+end
+
+function tbls = parseoutputs(tbls, wastabular, wasstruct, names)
+
+   if wasstruct
+      tbls = cell2struct(tbls, names, 1);
+
+   elseif wastabular
+      tbls = tbls{:};
    end
 end
