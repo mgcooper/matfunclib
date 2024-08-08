acabc55a13389262947a55d49bf4d02ad373fa28
mgcooper <mguycooper@gmail.com>
2024-08-08 00:16:31 -0700

    refactor(installRequiredFiles): Replace "testRun" with "dryrun".

--- toolbox/toolbox/+tbx/+internal/installRequiredFiles.m
+++ toolbox/toolbox/+tbx/+internal/installRequiredFiles.m
@@ -61,7 +61,7 @@ function [requirementsList, urlList] = installRequiredFiles(requiredFiles, kwarg
       kwargs.GitHubUserName (1, :) string {mustBeTextScalar} ...
          = getenv('GITHUB_USER_NAME')
 
-      kwargs.testRun (1, 1) logical {mustBeNumericOrLogical} ...
+      kwargs.dryrun (1, 1) logical {mustBeNumericOrLogical} ...
          = false
    end
 
@@ -83,7 +83,7 @@ function [requirementsList, urlList] = installRequiredFiles(requiredFiles, kwarg
 
    % Option to install the missing requirement locally
    fileList = kwargs.installPath + filesep + requirementsList;
-   if not(kwargs.testRun)
+   if not(kwargs.dryrun)
 
       if ~isfolder(kwargs.installPath)
          mkdir(kwargs.installPath)
@@ -98,8 +98,8 @@ function [requirementsList, urlList] = installRequiredFiles(requiredFiles, kwarg
          end
       end
    else
-      fprintf("\n Files will be installed to: \n %s \n", kwargs.installPath)
-      fprintf("\n The following files will be installed: \n")
+      fprintf(1, "\n Files will be installed to: \n %s \n", kwargs.installPath)
+      fprintf(1, "\n The following files will be installed: \n")
       disp(urlList)
    end
 end
