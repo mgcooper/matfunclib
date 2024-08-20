db6e6beafd594d070b514722b97db514af28784a
mgcooper <mguycooper@gmail.com>
2024-08-20 12:54:26 -0700

    feat(parsePolygonNames)!: Redesign for parsePolygons; add expectedNames, see notes.
    Redesigned and simplified the inputs so the first one is either a struct/table with a name field or the list of names, this way the function can be called directly from parsePolygons for the case where P is a shapefile, or with a list of names from wherever, and parsedPolygons is an optional input. Also made it analogous to parseFieldNames, so basically the difference is in parsing the "name" field rather than the fieldnames themselves.

--- libspatial/polygon/parsePolygonNames.m
+++ libspatial/polygon/parsePolygonNames.m
@@ -1,39 +1,80 @@
-function polygonNames = parsePolygonNames( ...
-      polygonNames, wasfileP, fromfileP, parsedP)
+function [names, found, extra, missing] = parsePolygonNames( ...
+      namedPolygons, parsedPolygons, expectedNames)
    %PARSEPOLYGONNAMES
    %
-   %  polygonNames = parsePolygonNames(polygonNames,wasfileP,fromfileP,parsedP)
+   %    names = parsePolygonNames(namedPolygons)
+   %    names = parsePolygonNames(namedPolygons, parsedPolygons)
+   %    [names, found, extra, missing] = parsePolygonNames(_, expectedNames)
    %
-   % Note: This function is meant to be called from, or in tandem with,
-   % parsePolygons. It uses the parsed P returned by parsePolygons. However,
-   % this function can also be used to retrieve the polygon names from a
-   % shapefile attribute table by providing an empty polygonNames input
-   % argument, and in this case, parsedP becomes optional.
+   %  Description
+   %    There are two use cases for this function:
+   %        * Validate that the names in a NAMEDPOLYGONS string or cellstr list
+   %        has one name per element of PARSEDPOLYGONS.
+   %        * Obtain NAMES from the 'name' field of a NAMEDPOLYGONS object.
+   %    In the first case, the assumption is that NAMEDPOLYGONS either was not
+   %    read-in from a shapefile and thus the named-field object representation
+   %    of POLYGONS (i.e., struct or table) is unavailable in the calling
+   %    workspace, or an alternative list of names which does not match the
+   %    'name' field is being used and you simply want to validate the lengths
+   %    match (add more use cases as needed).
+   %
+   %    In the second case, the assumption is that POLYGONS is the
+   %
+   %  Inputs
+   %     NAMEDPOLYGONS - a list of polygon names, or an object (struct or table)
+   %     with a 'name' field from which the polygon names can be retrieved.
+   %     PARSEDPOLYGONS - a list (cell array, polyshape vector) or struct of
+   %     polygons.
+   %
+   %    This function is meant to be called from, or in tandem with,
+   %    parsePolygons. It uses the parsed P returned by parsePolygons. However,
+   %    this function can also be used to retrieve the polygon names from a
+   %    shapefile attribute table by providing an empty polygonNames input
+   %    argument, and in this case, parsedP becomes optional.
    %
    % See also: parsePolygons
 
-   if isempty(polygonNames) && wasfileP
-      [found, name] = isfieldname(fromfileP, 'name');
+   if nargin < 3
+      expectedNames = string.empty(1, 0);
+   end
+   if nargin < 2
+      parsedPolygons = namedPolygons;
+   end
+
+   % If namedPolygons is a named-fields object, retrieve the 'name' field.
+   if isstruct(namedPolygons) || istable(namedPolygons)
+
+      [found, namefield] = isfieldname(namedPolygons, 'name');
       if found
-         polygonNames = string(...
-            matlab.lang.makeValidName({fromfileP.(string(name))}'));
+         names = string(...
+            matlab.lang.makeValidName({namedPolygons.(string(namefield))}'));
       else
-         polygonNames = string.empty(1, 0);
+         names = string.empty(1, 0);
       end
+   else
+      % Ensure that names is returned as a string array if it is a cellstr,
+      % char, or string array, otherwise it will be an empty string.
+      names = parseFieldNames(namedPolygons);
    end
 
-   % If parsedP was not provided, assign it to fromfileP so the warning passes.
-   if nargin < 3 || isempty(parsedP)
-      parsedP = fromfileP;
+   if isempty(expectedNames)
+      expectedNames = names;
    end
 
-   % Confirm the number of names found in "fromfileP" (nominally a shapefile
-   % attribute table) match the number of polygons returned by parsePolygons.
-   % This applies equally to provided polygonNames or found names.
-   if ~isempty(polygonNames) && numel(polygonNames) ~= numel(parsedP)
+   % If names and expectedNames are both empty strings, this ensures found,
+   % extra, and missing are also empty strings.
+   [names, found, extra, missing] = parseFieldNames(names, expectedNames);
+
+   % Confirm the number of names matches the number of parsed polygons
+   % (nominally returned by parsePolygons). This applies equally to names
+   % provided by namedPolygons in list format, or names retrieved from the
+   % 'name' field if namedPolygons was an object. 'size' is used so that
+   % parsedPolygons can be an Nx2 or 2xN cell array, or an Nx1 or 1xN polygon
+   % representation.
+   if ~any(numel(found) == size(parsedPolygons))
       warning( ...
          ['Number of polygon names does not match the number of polygons.' ...
          'Ignoring polygon names'])
-      polygonNames = string.empty(1, 0);
+      namedPolygons = string.empty(1, 0);
    end
 end
